f16f85d75641f20e04dd5fbc717a0877de493810
Velimir Müller <velimir@demos-deutschland.de>
2022-11-18 17:46:25 +0100

    add npm entrypoint and remove other index.js files
    - Since it appears to be problematic to have multiple entrypoints, this was changed to a single index.js
    - This means all demosplan-ui components can be imported directly via the package, without specifying the exact location of components or javascript files within the npm package

---
+++ index.js
@@ -0,0 +1,32 @@
+/**
+ * (c) 2010-present DEMOS E-Partizipation GmbH.
+ *
+ * This file is part of the package @demos-europe/demosplan-ui,
+ * for more information see the license file.
+ *
+ * All rights reserved
+ */
+import { Tooltip, VPopover } from './directives/Tooltip/Tooltip'
+import { CleanHtml } from './directives/CleanHtml/CleanHtml'
+import DpButton from './components/DpButton/DpButton'
+import DpDetails from './components/DpDetails/DpDetails'
+import DpIcon from './components/DpIcon/DpIcon'
+import DpInput from './components/DpInput/DpInput'
+import DpLabel from './components/DpLabel/DpLabel'
+import DpLoading from './components/DpLoading/DpLoading'
+import prefixClass from './lib/prefixClass'
+import prefixClassMixin from './mixins/prefixClassMixin'
+
+export {
+  CleanHtml,
+  DpButton,
+  DpDetails,
+  DpIcon,
+  DpInput,
+  DpLabel,
+  DpLoading,
+  prefixClass,
+  prefixClassMixin,
+  Tooltip,
+  VPopover
+}

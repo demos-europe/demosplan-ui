0adb789914b45e389587f5443453fd1a5df25aea
Hans Spieß <spiess@demos-deutschland.de>
2022-12-16 11:24:39 +0100

    fix(refs T29774: allow target blank
    As we allow the target attr, we have to make sure that the link is safe.
    Chromium seems to add `rel="noopener"` automatically as of v88, however
    to also ensure correct behavior for other engines, the `rel` attr is added
    whenever a target is set to "_blank".

--- src/directives/CleanHtml/CleanHtml.js
+++ src/directives/CleanHtml/CleanHtml.js
@@ -1,6 +1,24 @@
 import DOMPurify from 'dompurify'
 
 /**
+ * As we allow the target attr, we have to make sure that the link is safe.
+ * Chromium seems to add `rel="noopener"` automatically as of v88, however
+ * to also ensure correct behavior for other engines, the `rel` attr is added
+ * whenever a target is set to "_blank".
+ *
+ * @see https://developer.chrome.com/docs/lighthouse/best-practices/external-anchors-use-rel-noopener/
+ */
+DOMPurify.addHook('afterSanitizeAttributes', function (node) {
+  if (!'target' in node) {
+    return
+  }
+  const target = node.getAttribute('target')
+  if (target === '_blank') {
+    node.setAttribute('rel', 'noopener')
+  }
+})
+
+/**
  * Checks the type of value and always returns an object to be used with setInnerHTML.
  * If a string is passed, its value is used as content.
  * @param value {Object<content, options>|String}
@@ -24,7 +42,8 @@ const getOptions = (value) => {
  */
 const setSanitizedInnerHTML = (el, binding) => {
   const { content, options = {} } = getOptions(binding.value)
-  el.innerHTML = DOMPurify.sanitize(content, options)
+  const _options = {...options, ...{ ADD_ATTR: ['target'] } }
+  el.innerHTML = DOMPurify.sanitize(content, _options)
 }
 
 const CleanHtml = {

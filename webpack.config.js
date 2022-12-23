0530bcad5fa96e06e3bfa6666b3078dd4aea8998
Hans Spieß <spiess@demos-deutschland.de>
2022-12-23 16:00:18 +0100

    feat(refs T30598): WIP test dynamic imports 2a

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,9 @@ function resolve (dir) {
 
 const config = {
   entry: resolve('./src/index.js'),
+  experiments: {
+    topLevelAwait: true
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'demosplan-ui.umd.js',

d90277fdb90ee4349783e5c1e51137121a6e26c6
Ahmad Alahrashi <alahrashi@demos-deutschland.de>
2023-01-20 16:46:32 +0100

    chore (refs T30968): remove some lib files, move it to demosplan-core.

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,60 +1,34 @@
-import { addFormHiddenField, FormActions, removeFormHiddenField } from './FormActions'
 import { checkResponse, dpApi, dpRpc, handleResponseMessages, makeFormPost } from './DpApi'
 import { convertSize, getFileInfo, getFileTypes, mimeTypes } from './FileInfo'
 import { highlightActiveLinks } from './HighlightHashLink'
 import ActionMenu from './ActionMenu'
-import AnimateById from './AnimateById'
-import CharCount from './CharCount'
-import CheckableItem from './CheckableItem'
 import Confirm from './DpConfirm'
 import Detabinator from './Detabinator'
-import FloodControlField from './FloodControlField'
 import getCssVariable from './DpGetCssVariable'
-import initGlobalEventListener from './GlobalEventListener'
 import MatchMedia from './MatchMedia'
 import SideNav from './SideNav'
 import Stickier from './Stickier'
 import Sticky from './Sticky'
 import TableWrapper from './TableWrapper'
-import Tabs from './Tabs'
-import ToggleAnything from './ToggleAnything'
-import ToggleSideMenu from './ToggleSideMenu'
-import Tooltips from './Tooltips'
-import touchFriendlyUserbox from './touchFriendlyUserbox'
-import Pager from './Pager'
 
 export {
   ActionMenu,
-  addFormHiddenField,
-  AnimateById,
-  CharCount,
   checkResponse,
-  CheckableItem,
   Confirm,
   convertSize,
   Detabinator,
   dpApi,
   dpRpc,
-  FloodControlField,
-  FormActions,
   getCssVariable,
   getFileInfo,
   getFileTypes,
   handleResponseMessages,
   highlightActiveLinks,
-  initGlobalEventListener,
   makeFormPost,
   MatchMedia,
   mimeTypes,
-  removeFormHiddenField,
   SideNav,
   Stickier,
   Sticky,
   TableWrapper,
-  Tabs,
-  ToggleAnything,
-  ToggleSideMenu,
-  Tooltips,
-  touchFriendlyUserbox,
-  Pager
 }

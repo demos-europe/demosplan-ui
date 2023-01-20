ab15a681b93e0bb52283d5769ac4a5fd9b7030a6
Ahmad Alahrashi <alahrashi@demos-deutschland.de>
2023-01-20 13:19:09 +0100

    chore (refs T30968): Edit path.

--- src/components/core/DpEditor/DpRecommendationModal.vue
+++ src/components/core/DpEditor/DpRecommendationModal.vue
@@ -57,7 +57,7 @@
 
 <script>
 import { mapMutations, mapState } from 'vuex'
-import { dpApi } from '@demos-europe/demosplan-ui'
+import { dpApi } from '../../../lib'
 import dataTableSearch from '../DpDataTable/DataTableSearch'
 import DpLabel from '../../DpLabel/DpLabel'
 import DpLoading from '../../DpLoading/DpLoading'

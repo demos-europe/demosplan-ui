357eb0f33d77a863d39f4bbd04aa3dc377ffd1a0
ahmad-demos <59571234+ahmad-demos@users.noreply.github.com>
2023-04-13 15:00:03 +0200

    F t30974 continue with color brand UI (#105)
    * chore (refs T31116): edit color value.
    
    * chore (refs T31116): Add new color (teal) to palette.
    
    * chore (refs T31116): change hex colors to lowercase.
    
    * chore (refs T31116): edit (magenta) hex color.
    
    * chore (refs T30974): add alt variable to brand.
    
    * chore (refs T30974): add highlight variable to brand.
    
    * chore (refs T30974): make color.json file as a separate file
    
    - add all separate colors file in color folder
    - minor improvements in buildTokens.js
    
    * chore (refs T30974): add new values for color brand.
    
    * feat(refs T25032): add more color tokens
    
    We have agreed on moving everything that is not brand,
    or palette/data, into "ui" tokens file.
    The "ui" part, however, is removed when rendering those tokens
    as scss files, to make them more legible and shorter.
    
    * chore (refs T30974): add comment.
    
    * feat(refs T25032): remove the "header", "cta", and "nav-alt" token
    
    these should not be part of demosplan-ui as they are not used within
    components, rather describe very specific elements within the public
    area of demosPlan.
    
    * feat(refs T25032): minor docs tweak
    
    * docs(refs T25032): add usage examples for color tokens
    
    * docs(refs T25032): add overview page
    
    * feat(refs T30974): add text color tokens
    
    * feat(refs T30974): tweak token generation
    
    - drop "palette" or "ui" as well as "scale", "brand" or "heading" from variable name
    - add status tokens for internal usage
    
    * feat(refs T30974): add more tokens, restructuring
    
    for main, alt, highlight: drop "base" again, it would
    imply that there are shades in these tokens.
    
    tweak red base hex value, make it a little less aggressive.
    
    add a bunch of tokens bc they're needed.
    
    * feat(refs T30974): adapt utility classes to core changes
    
    as css atm is generated in demosplan-core, this has to be done
    in such a way for now... looking forward to define utility classes
    within demosplan-ui soon.
    
    * feat(refs T30974): remove obsolete todo
    
    * feat(refs T30974): add tbd note
    
    * feat(refs T30974): revert font variable naming
    
    * feat(refs T30974): choose verbose variable name for font variables
    
    ---------
    
    Co-authored-by: Hans Spieß <spiess@demos-deutschland.de>

--- buildTokens.js
+++ buildTokens.js
@@ -3,19 +3,22 @@ const StyleDictionary = require('style-dictionary')
 
 const prefix = 'dp-'
 
-const tokensPath = 'tokens/*.json'
+const tokensPath = 'tokens/**/*.json'
 const files = glob
   .sync(tokensPath)
   .map(filePath => filePath
     .replace('tokens/', '')
+    .replace('color/', '')
     .replace('.json', ''))
+  // Do not render tokens only used internally
+  .filter(filePath => !filePath.startsWith('_'))
 
 StyleDictionary.registerTransform({
   name: 'name/scss',
   type: 'name',
   transformer: (token) => {
-    // "palette" within colors should not be part of the variable name
-    if (token.path[0] === 'color' && token.path[1] === 'palette') {
+    // "palette" or "ui" within colors should not be part of the variable name.
+    if (token.path[0] === 'color' && /(palette|ui)/.test(token.path[1])) {
       token.path.splice(1, 1)
     }
     return prefix + token.path.join('-')

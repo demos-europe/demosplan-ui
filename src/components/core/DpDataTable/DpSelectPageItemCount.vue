13c7819d595ef914719d6edc996268bda0238f82
Florian Salis <salis@demos-deutschland.de>
2023-05-03 21:53:29 +0200

    chore(refs T19787, T19759): Rebuild template for selectPageItemCount

--- src/components/core/DpDataTable/DpSelectPageItemCount.vue
+++ src/components/core/DpDataTable/DpSelectPageItemCount.vue
@@ -1,8 +1,25 @@
+<template>
+  <div>
+    <select
+      id="item-count"
+      class="o-form__control-select width-auto u-mr-0_25"
+      @change="e => $emit('change-count', parseInt(e.target.value))">
+      <option
+        v-for="option in pageCountOptions"
+        :value="option"
+        :selected="option === currentItemCount" />
+    </select>
+    <label
+      for="item-count"
+      class="display--inline u-mb-0">
+      {{ translations.pagerElementsPerPage }}
+    </label>
+  </div>
+</template>
+
 <script>
 export default {
   name: 'DpSelectPageItemCount',
-
-  functional: true,
 
   props: {
     currentItemCount: {
@@ -19,43 +36,6 @@ export default {
       type: Object,
       required: true
     }
-  },
-
-  render: function (h, { props, listeners, data }) {
-    const selectEl = h('select', {
-      attrs: {
-        id: 'item-count',
-        class: 'o-form__control-select width-auto u-mr-0_25'
-      },
-      on: {
-        change: (e) => {
-          listeners['changed-count'](parseInt(e.target.value))
-        }
-      }
-    }, [
-      ...props.pageCountOptions.map(option => {
-        return h('option', {
-          domProps: {
-            value: option,
-            selected: option === props.currentItemCount
-          }
-        }, option)
-      })
-    ])
-
-    const selectHintEl = h('label', {
-      attrs: {
-        for: 'item-count',
-        class: 'display--inline u-mb-0'
-      }
-    },
-    props.translations.pagerElementsPerPage)
-
-    return h('div', {
-      attrs: {
-        class: data.staticClass
-      }
-    }, [selectEl, selectHintEl])
   }
 }
 </script>

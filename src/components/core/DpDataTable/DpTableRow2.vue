cc7f72f92fc9d03d1f17cd83ef6d7efca5be1c1b
Natalija Sakuta <sakuta@demos-deutschland.de>
2023-05-10 14:27:17 +0200

    refactor: DpTableRow functional component into stateful component

---
+++ src/components/core/DpDataTable/DpTableRow2.vue
@@ -0,0 +1,253 @@
+<template>
+  <tr
+    class="row"
+    :class="[{ 'opacity-7': isLoading }, { 'is-expanded-row': expanded }]">
+
+    //isDraggable
+    <td v-if="isDraggable"
+        class="c-data-table__cell--narrow">
+      <dp-icon icon="drag-handle" />
+    </td>
+
+    //isSelectable
+    <td v-if="isSelectable"
+        class="c-data-table__cell--narrow">
+      <dp-icon
+          v-if="isLocked"
+          class="u-valign--middle color--grey-light"
+          v-tooltip="isLockedMessage"
+          icon="lock" />
+      <input
+          v-else
+          type="checkbox"
+          class="u-m-0 u-valign--middle"
+          data-cy="selectItem"
+          :name="isSelectableName || null"
+          :value="isSelectableName ? item[trackBy] : null"
+          :checked="checked"
+          @click="$listeners.toggleSelect(item[trackBy])">
+    </td>
+
+    //Fields
+    <template
+        v-for="(field, idx) in fields"
+        :key="`${field}:${idx}`">
+      <td
+        :class="{ 'c-data-table__resizable': isTruncatable }"
+        :data-col-idx="`${idx}`">
+        <div
+          v-if="isTruncatable"
+          :class="wrapped ? 'c-data-table__resizable--wrapped overflow-word-break' : 'c-data-table__resizable--truncated overflow-word-break'"
+          :style="elementStyle(field)">
+          <slot :name="field" :item="item">
+            <span
+              v-if="searchTerm && item[field]"
+              v-html="highlight(field)" />
+            <span v-else>
+              {{ item[field] }}
+            </span>
+          </slot>
+        </div>
+        <template v-else>
+          <slot :name="field" :item="item" />
+        </template>
+      </td>
+    </template>
+
+    //hasFlyout
+    <td v-if="hasFlyout"
+        class="overflow-visible">
+      <slot name="flyout" :item="item" />
+    </td>
+
+    ///isExpandable
+    <td v-if="isExpandable"
+      class="c-data-table__cell--narrow"
+      :class="{'is-open': expanded }"
+      :title="Translator.trans(expanded ? 'aria.collapse' : 'aria.expand')"
+      @click="$listeners.toggleExpand(item[trackBy])">
+      <dp-wrap-trigger :expanded="expanded" />
+    </td>
+
+    //isTruncatable
+    <td class="c-data-table__cell--narrow"
+        :class="{'is-open': wrapped}"
+        :title="Translator.trans(wrapped ? 'aria.collapse' : 'aria.expand')"
+        @click="$listeners.toggleWrap(item[trackBy])">
+      <dp-wrap-trigger :expanded="wrapped" />
+
+    </td>
+  </tr>
+</template>
+
+<script>
+import DpIcon from '../../DpIcon/DpIcon'
+import DpWrapTrigger from './DpWrapTrigger'
+import DomPurify from 'dompurify'
+export default {
+  name: "DpTableRow2.vue",
+
+  components: {
+    DpIcon,
+    DpWrapTrigger
+  },
+
+  props: {
+    checked: {
+      type: Boolean,
+      required: true
+    },
+
+    /**
+     * Is the expandable content currently expanded?
+     */
+    expanded: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+
+    hasFlyout: {
+      type: Boolean,
+      required: true
+    },
+
+    /**
+     * The header of every column of the table is defined here.
+     *
+     * Each column is represented by an object with a `field` key whose value should match
+     * a key of the objects inside `items`. The `label` key controls the header of the column.
+     * The header can also have a tooltip. To define the width the column is initially
+     * rendered with when `isResizable` is used, the key `initialWidth` takes a px value.
+     */
+    headerFields: {
+      type: Array,
+      required: true
+    },
+
+    fields: {
+      type: Array,
+      required: true
+    },
+
+    item: {
+      type: Object,
+      required: true
+    },
+
+    isDraggable: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+
+    isExpandable: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+
+    isLoading: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+
+    /**
+     * The item can be locked for selection. Instead of the checkbox, a lock icon is rendered with a tooltip.
+     */
+    isLocked: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+
+    /**
+     * If an item is locked for selection, the message to be shown inside the tooltip can be set here.
+     */
+    isLockedMessage: {
+      type: String,
+      required: false,
+      default: null
+    },
+
+    isResizable: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+
+    isSelectable: {
+      type: Boolean,
+      required: true
+    },
+
+    isSelectableName: {
+      type: String,
+      required: false,
+      default: null
+    },
+
+    isTruncatable: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+
+    searchTerm: {
+      type: RegExp,
+      required: true
+    },
+
+    trackBy: {
+      type: String,
+      required: true
+    },
+
+    /**
+     * Is the truncatable content currently truncated?
+     * Rename to `notTruncated`?
+     */
+    wrapped: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+
+  data () {
+    return {
+      highlighted: null
+    }
+  },
+
+  computed: {
+    highlight () {
+      return (field) => {
+        let itemValue = this.item[field]
+        itemValue = DomPurify.sanitize(itemValue)
+        const regex = new RegExp(this.searchTerm, 'gi')
+        this.highlighted = itemValue.replace(regex, (match) => `<span style="background-color: yellow;">${match}</span>`)
+      }
+    },
+    elementStyle () {
+      return (field) => {
+        const headerField = this.headerFields.find((hf) => hf.field === field)
+        let style = ''
+
+        if (!this.wrapped && typeof headerField.initialWidth !== 'undefined') {
+          style += `width: ${headerField.initialWidth}px;`
+        }
+        if (!this.wrapped && typeof headerField.initialMaxWidth !== 'undefined') {
+          style += `max-width: ${headerField.initialMaxWidth}px;`
+        }
+        if (!this.wrapped && typeof headerField.initialMinWidth !== 'undefined') {
+          style += `min-width: ${headerField.initialMinWidth}px;`
+        }
+
+        return style
+      }
+    }
+  }
+}
+</script>

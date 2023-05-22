3c7ebaf898909d13448c206578cae0233e443385
Stefanie Grünberger <gruenberger@demos-deutschland.de>
2023-05-22 09:19:13 +0200

    bug(refs T32834): Change page props for pager to strings
    Multiselect expects strings now instead of numbers.

--- src/components/DpPager/DpPager.vue
+++ src/components/DpPager/DpPager.vue
@@ -4,7 +4,7 @@
       class="c-pager__dropdown-label u-m-0 u-p-0 weight--normal display--inline-block"
       :aria-label="Translator.trans('pager.amount.multiple.label', { results: totalItems, items: Translator.trans('pager.amount.multiple.items') })">
         <dp-sliding-pagination
-          v-if="totalItems > Math.min(...limits)"
+          v-if="Number(totalItems) > Math.min(...limits)"
           class="display--inline-block"
           :current="currentPage"
           :nonSlidingSize="3"
@@ -14,7 +14,7 @@
           @page-change="handlePageChange" />
       <div
         class="display--inline-block"
-        v-if="totalItems > Math.min(...limits)">
+        v-if="Number(totalItems) > Math.min(...limits)">
         <dp-multiselect
           class="display--inline-block"
           v-model="itemsPerPage"
@@ -54,8 +54,8 @@ export default {
 
     totalItems: {
       required: false,
-      type: Number,
-      default: 1
+      type: String,
+      default: '1'
     },
 
     totalPages: {
@@ -66,8 +66,8 @@ export default {
 
     perPage: {
       required: false,
-      type: Number,
-      default: 1
+      type: String,
+      default: '1'
     },
 
     limits: {
@@ -79,15 +79,15 @@ export default {
 
   data () {
     return {
-      itemsPerPage: this.perPage <= this.totalItems ? this.perPage.toString() : this.totalItems.toString()
+      itemsPerPage: Number(this.perPage) <= Number(this.totalItems) ? this.perPage : this.totalItems
     }
   },
 
   computed: {
     filteredLimits () {
-      const filtered = this.limits.filter(limit => limit <= this.totalItems)
+      const filtered = this.limits.filter(limit => limit <= Number(this.totalItems))
 
-      if (filtered.length < this.limits.length && this.totalItems > filtered[filtered.length - 1]) {
+      if (filtered.length < this.limits.length && Number(this.totalItems) > filtered[filtered.length - 1]) {
         filtered.push(this.totalItems)
       }
 

79fe7f2772008ba30e0de73ebec8ea871e98fabd
Florian Salis <salis@demos-deutschland.de>
2023-06-01 16:18:44 +0200

    test(refs T30971): Setup for Vue tests
    - make Vue files testable
    - since we are at vue2 , we have to stick to @vue/test-utils@1

--- jest.config.js
+++ jest.config.js
@@ -10,37 +10,18 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 const path = require('path')
 
+console.dir('<rootDir>')
 module.exports = {
-  displayName: 'demosplan-ui',
   collectCoverage: false,
   collectCoverageFrom: [
-      './utils/*.{js}',
-      './mixins/*.{js}',
-      './lib/*.{js}',
+      './src/utils/*.{js}',
+      './src/mixins/*.{js}',
+      './src/lib/*.{js}',
+      './src/components/*.{vue}'
   ],
-  coverageReporters: ['clover', 'json', 'lcov', ['text', { skipFull: true }]],
   coverageDirectory: './coverage',
-  moduleFileExtensions: ['js', 'ts', 'vue'],
-  modulePathIgnorePatterns: [
-    './dist'
-  ],
-  testRegex: './(src|utils)/tests/.*(test|spec)\\.js]?$',
-  rootDir: path.resolve(__dirname, './'),
-  roots: ['./src'],
-  modulePaths: ['./src'],
-  moduleDirectories: [
-    'node_modules'
-  ],
-  transform: {
-      '.*\\.js$': './babel-jest-transformer.js',
-      '.*\\.vue$': '@vue/vue2-jest',
-      '^.+\\.tsx?$': 'ts-jest',
-  },
-  transformIgnorePatterns: [
-    'node_modules/(?!@tiptap)(.*)'
-  ],
-  verbose: true,
-  testEnvironment: "jsdom",
+  coverageReporters: ['clover', 'json', 'lcov', ['text', { skipFull: true }]],
+  displayName: 'demosplan-ui',
   globals: {
     '@vue/vue2-jest': {
       babelConfig: {
@@ -48,10 +29,38 @@ module.exports = {
       }
     }
   },
+  moduleDirectories: [
+    'node_modules'
+  ],
+  moduleFileExtensions: [
+    'js',
+    'ts',
+    'vue'
+  ],
+  modulePaths: [
+    '<rootDir>'
+  ],
+  modulePathIgnorePatterns: [
+    './dist'
+  ],
+  rootDir: path.resolve(__dirname, './'),
+  roots: ['./tests'],
+  setupFiles: ['./jest/setup.js'],
+  testRegex: '.*(test|spec)\\.js]?$',
+  transform: {
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.(vue)$': '<rootDir>/node_modules/@vue/vue2-jest',
+    '^.+\\.(ts)$': '<rootDir>/node_modules/ts-jest'
+  },
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(?!@tiptap)(.*)'
+  ],
+  testEnvironment: "jsdom",
   testEnvironmentOptions: {
     customExportConditions: [
       'node',
       'node-addons'
     ]
-  }
+  },
+  verbose: true
 }

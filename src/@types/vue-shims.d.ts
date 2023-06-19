845e7d897d5e9327376e05684304ab6ad1911211
ahmad-demos <59571234+ahmad-demos@users.noreply.github.com>
2023-06-19 10:00:01 +0200

    Update src/@types/vue-shims.d.ts
    Co-authored-by: spiess-demos <40452344+spiess-demos@users.noreply.github.com>

--- src/@types/vue-shims.d.ts
+++ src/@types/vue-shims.d.ts
@@ -1,4 +1,11 @@
-declare module "*.vue" {
+/**
+ * Explain to typescript what Vue components are.
+ */
+declare module '*.vue' {
+    import type { DefineComponent } from 'vue'
+    const component: DefineComponent<{}, {}, any>
+    export default component
+}
     import Vue from "vue";
     export default Vue;
 }

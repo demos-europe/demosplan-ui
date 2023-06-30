7801be77676b107701af5420ff35dd63a953a543
Florian Salis <salis@demos-deutschland.de>
2023-06-30 23:07:30 +0200

    refactor: Build demosplan-ui as "real" module
    but since we don't only want to export defaults we have to be a bit creative

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+import * as shared from './shared'
+import * as directives from './directives'
+import * as utils from './utils'
+import * as components from './components/'
+import * as mixins from './mixins/'
+import * as lib from './lib/'
+import * as validation from './lib/validation/'
+
 export * from './shared'
 export * from './directives'
 export * from './utils'
@@ -6,3 +14,13 @@ export * from './mixins/'
 export * from './lib/'
 export * from './lib/validation/'
 
+
+export default {
+  ...shared,
+  ...directives,
+  ...utils,
+  ...components,
+  ...mixins,
+  ...lib,
+  ...validation
+}

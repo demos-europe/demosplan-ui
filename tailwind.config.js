5a845e4f8f9157113e7c62e3c2ebad5596413bb6
Hans Spieß <spiess@demos-deutschland.de>
2023-07-11 13:39:24 +0200

    feat(ref T27596): refactor out transform fn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,22 @@
 const plugin = require('tailwindcss/plugin')
 
-const borderRadius = Object.fromEntries(
-  Object.values(require('./tokens/dist/js/rounded').rounded)
-    // Filter out aliases here because they are only included for backward compatibility with Scss
-    .filter(({ original }) => original.value.includes('{') === false)
-    .map(({ attributes, value }) => [attributes.type, value])
-)
+/**
+ * Transform a StyleDictionary js module representation of the tokens object
+ * into a simplified object to be consumable for the Tailwind config.
+ * It filters out tokens that are aliases for other tokens.
+ * @param tokens The original object
+ * @return {{[p: string]: unknown}}
+ */
+const tokensToTailwind = (tokens) => {
+  const tokensArray = Object.values(tokens)
+  const filteredTokens = tokensArray.filter(({ original }) => original.value.includes('{') === false)
+  return Object.fromEntries(filteredTokens.map(({ attributes, value }) => [attributes.type, value]))
+}
 
-const boxShadow = Object.fromEntries(
-  Object.values(require('./tokens/dist/js/boxShadow')['box-shadow'])
-    // Filter out aliases here because they are only included for backward compatibility with Scss
-    .filter(({ original }) => original.value.includes('{') === false)
-    .map(({ attributes, value }) => [attributes.type, value])
-)
-
-const screens = Object.fromEntries(
-  Object.values(require('./tokens/dist/js/breakpoints').breakpoints).map(
-    ({ attributes, value }) => [attributes.type, value]
-  )
-)
-
-const zIndex = Object.fromEntries(
-  Object.values(require('./tokens/dist/js/zIndex')['z-index']).map(
-    ({ attributes, value }) => [attributes.type, value]
-  )
-)
+const borderRadius = tokensToTailwind(require('./tokens/dist/js/rounded').rounded)
+const boxShadow = tokensToTailwind(require('./tokens/dist/js/boxShadow')['box-shadow'])
+const screens = tokensToTailwind(require('./tokens/dist/js/breakpoints').breakpoints)
+const zIndex = tokensToTailwind(require('./tokens/dist/js/zIndex')['z-index'])
 
 module.exports = {
   content: [

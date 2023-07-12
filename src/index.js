aa2b8a988df4d8e6310055fecc631c9fe3655990
Florian Salis <salis@demos-deutschland.de>
2023-07-12 14:04:18 +0200

    chore: alphabetic order

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-export * from './shared'
+export * from './components'
 export * from './directives'
+export * from './lib'
+export * from './mixins'
+export * from './shared'
 export * from './utils'
-export * from './components/'
-export * from './mixins/'
-export * from './lib/'
-export * from './lib/validation/'
+export * from './lib/validation'
 
-import * as shared from './shared'
+import * as components from './components'
 import * as directives from './directives'
+import * as lib from './lib'
+import * as mixins from './mixins'
+import * as shared from './shared'
 import * as utils from './utils'
-import * as components from './components/'
-import * as mixins from './mixins/'
-import * as lib from './lib/'
-import * as validation from './lib/validation/'
+import * as validation from './lib/validation'
 
 export default {
-  ...shared,
-  ...directives,
-  ...utils,
   ...components,
-  ...mixins,
+  ...directives,
   ...lib,
+  ...mixins,
+  ...shared,
+  ...utils,
   ...validation
 }

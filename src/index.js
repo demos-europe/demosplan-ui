6d4b78ad0065491cd684957081e56c5d174d6573
Florian Salis <salis@demos-deutschland.de>
2023-07-12 13:55:30 +0200

    Fix: Add a default export to index.js
    For some reasons we need both. Named and default exports

--- src/index.js
+++ src/index.js
@@ -6,3 +6,20 @@ export * from './mixins/'
 export * from './lib/'
 export * from './lib/validation/'
 
+import * as shared from './shared'
+import * as directives from './directives'
+import * as utils from './utils'
+import * as components from './components/'
+import * as mixins from './mixins/'
+import * as lib from './lib/'
+import * as validation from './lib/validation/'
+
+export default {
+  ...shared,
+  ...directives,
+  ...utils,
+  ...components,
+  ...mixins,
+  ...lib,
+  ...validation
+}

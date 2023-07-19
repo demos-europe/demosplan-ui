74e04e208a4e6b0923671b1b14bcbb4b0d407919
Florian Salis <salis@demos-deutschland.de>
2023-07-19 11:53:39 +0200

    chore: Migrate DpContextualHelp stories to v7
    ....to learn what to do.

---
+++ src/components/DpContextualHelp/DpContextualHelp.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/vue'
+
+import DpContextualHelp from './DpContextualHelp.vue'
+
+const Translator = {
+  trans: (key: string): string => key
+}
+
+const meta: Meta<typeof DpContextualHelp> = {
+  component: DpContextualHelp
+}
+
+export default meta
+type Story = StoryObj<typeof DpContextualHelp>
+
+export const Default: Story = {
+  args: {
+    text: 'test text'
+  }
+}

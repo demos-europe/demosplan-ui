1bfdd23baf0396376672a8c3939b2a513ffe50bc
Florian Salis <salis@demos-deutschland.de>
2023-07-21 20:48:50 +0200

    chore: Roleback styling support for Storybook
    it only works with storybook7

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,17 +8,7 @@ module.exports = {
 
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    {
-      name: '@storybook/addon-styling',
-      options: {
-        // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
-        // For more details on this addon's options.
-        postCss: {
-          implementation: require.resolve('postcss')
-        }
-      }
-    }
+    "@storybook/addon-essentials"
   ],
 
   webpackFinal: async config => {

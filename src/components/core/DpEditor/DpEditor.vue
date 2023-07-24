435f3aea1ae045171132e862be58baa0ffcccb5d
salisdemos <40487461+salisdemos@users.noreply.github.com>
2023-07-24 14:18:26 +0200

    refactor(refs T33194, T19759): Add Obscure and insertAtCursorPos (#341)

--- src/components/core/DpEditor/DpEditor.vue
+++ src/components/core/DpEditor/DpEditor.vue
@@ -689,7 +689,7 @@ export default {
         Heading.configure({ levels: this.toolbar.headings })
       ]
 
-      // extensions.push(EditorInsertAtCursorPos)
+      extensions.push(EditorInsertAtCursorPos)
 
       if (this.suggestions.length > 0) {
       this.suggestions.forEach(suggestion => {
@@ -719,7 +719,7 @@ export default {
       }
 
       if (this.toolbar.obscure) {
-        // extensions.push(EditorObscure)
+        extensions.push(EditorObscure)
       }
 
       if (this.toolbar.listButtons) {

33d2595c683e15831ee3400ef0d2ce77ed2158be
Ahmad Alahrashi <alahrashi@demos-deutschland.de>
2023-07-24 15:09:00 +0200

    chore: WIP: Migrate DpAccordion stories to v7.

---
+++ src/components/DpAccordion/DpAccordion.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/vue'
+
+import DpAccordion from './DpAccordion.vue'
+
+const meta: Meta<typeof DpAccordion> = {
+    component: DpAccordion,
+    argTypes: {
+        'item:toggle': { action: 'item:toggle' }
+    }
+}
+
+export default meta
+type Story = StoryObj<typeof DpAccordion>
+
+export const Default: Story = {
+    args: {
+        fontWeight: 'bold',
+        compressed: true,
+        isOpen: false,
+        title: 'Accordion',
+
+    }
+}

10595c8b63347e480213fa40c255917f1da4fc26
Florian Salis <salis@demos-deutschland.de>
2023-07-24 13:34:41 +0200

    chore: Use abstraced import for dpAnonymizeText

--- src/components/core/DpAnonymizeText.vue
+++ src/components/core/DpAnonymizeText.vue
@@ -36,24 +36,27 @@ import {
   Editor, // Wrapper for prosemirror state
   EditorContent, // Renderless content element
 } from '@tiptap/vue-2'
-import Bold from '@tiptap/extension-bold'
-import BulletList from '@tiptap/extension-bullet-list'
-import Document from '@tiptap/extension-document'
-import HardBreak from '@tiptap/extension-hard-break'
-import Heading from '@tiptap/extension-heading'
-import History from '@tiptap/extension-history'
-import Italic from '@tiptap/extension-italic'
-import Link from '@tiptap/extension-link'
-import OrderedList from '@tiptap/extension-ordered-list'
-import Paragraph from '@tiptap/extension-paragraph'
-import Text from '@tiptap/extension-text'
-import Underline from '@tiptap/extension-underline'
-import ListItem from '@tiptap/extension-list-item'
-import Anonymize from './DpEditor/libs/editorAnonymize'
-import Obscure from './DpEditor/libs/editorObscure'
-import UnAnonymize from './DpEditor/libs/editorUnAnonymize'
-import eventPreventer from './DpEditor/libs/preventEditing'
-
+import {
+  Bold,
+  BulletList,
+  Document,
+  HardBreak,
+  Heading,
+  History,
+  Italic,
+  Link,
+  OrderedList,
+  Paragraph,
+  Text,
+  Underline,
+  ListItem
+} from './DpEditor/libs/tiptapExtensions'
+import {
+  Anonymize,
+  eventPreventer,
+  Obscure,
+  UnAnonymize
+} from './DpEditor/libs/customExtensions'
 export default {
   name: 'DpAnonymizeText',
 

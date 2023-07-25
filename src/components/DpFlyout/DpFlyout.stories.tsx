648a14b0ef88ada0b9924ed8bb09b81842e4f5cd
Ahmad Alahrashi <alahrashi@demos-deutschland.de>
2023-07-25 10:41:48 +0200

    chore: Migrate DpFlyout stories to v7.

---
+++ src/components/DpFlyout/DpFlyout.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/vue'
+
+import DpFlyout from './DpFlyout.vue'
+
+const meta: Meta<typeof DpFlyout> = {
+    component: DpFlyout,
+    argTypes: {
+        'open': { action: 'open' },
+        'close': { action: 'close' }
+    }
+}
+
+export default meta
+type Story = StoryObj<typeof DpFlyout>
+
+export const Default: Story = {
+    args: {
+        align: 'right',
+        disabled: false,
+        hasMenu: true,
+        padded: true
+    }
+}

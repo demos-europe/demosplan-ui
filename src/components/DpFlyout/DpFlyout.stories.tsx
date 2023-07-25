c4217ced0a82f315675c42d9f3f984656ea05529
Ahmad Alahrashi <alahrashi@demos-deutschland.de>
2023-07-25 11:59:43 +0200

    chore: add description to DpFlyout story.

--- src/components/DpFlyout/DpFlyout.stories.tsx
+++ src/components/DpFlyout/DpFlyout.stories.tsx
@@ -4,6 +4,15 @@ import DpFlyout from './DpFlyout.vue'
 
 const meta: Meta<typeof DpFlyout> = {
     component: DpFlyout,
+    parameters: {
+        docs: {
+            description: {
+                component: 'The Flyout component renders a flyout that is intended to show further content.\n' +
+                    '## Roadmap.\n' +
+                    '- use https://popper.js.org/docs/v2/ for positioning instead of basic css - or find another way to make left/right offset dynamic.'
+            },
+        },
+    },
     argTypes: {
         'open': { action: 'open' },
         'close': { action: 'close' }

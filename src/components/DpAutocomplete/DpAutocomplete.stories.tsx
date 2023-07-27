4eb9272323309aebcc8815f82bdc59ac21503e3e
Ahmad Alahrashi <alahrashi@demos-deutschland.de>
2023-07-27 14:08:53 +0200

    chore: Migrate DpAutocomplete stories to v7.

---
+++ src/components/DpAutocomplete/DpAutocomplete.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/vue'
+
+import DpAutocomplete from './DpAutocomplete.vue'
+
+const meta: Meta<typeof DpAutocomplete> = {
+    component: DpAutocomplete,
+    title: "Components/DpAutocomplete",
+    argTypes: {
+        'searchChanged': { action: 'searchChanged' }
+    }
+}
+
+export default meta
+type Story = StoryObj<typeof DpAutocomplete>
+
+export const Default: Story = {
+    args: {
+        height: '28px',
+        label: 'label',
+        routeGenerator: function () {},
+    }
+}

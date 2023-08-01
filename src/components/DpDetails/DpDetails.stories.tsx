1581d3e8c97dff6e697604cf6516e24f758eb4dc
Ahmad Alahrashi <alahrashi@demos-deutschland.de>
2023-08-01 15:46:54 +0200

    chore: Migrate DpDetails stories to v7.

---
+++ src/components/DpDetails/DpDetails.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/vue'
+import DpDetails from './DpDetails.vue'
+
+const meta: Meta<typeof DpDetails> = {
+    component: DpDetails,
+    title: "Components/Details",
+    argTypes: {},
+    render: (args) => ({
+        components: {
+            DpDetails,
+        },
+        setup() {
+            return { args }
+        },
+        template: `<dp-details primary secondary v-bind="args">
+        Content to be revealed onClick of the summary.
+        </dp-details>`,
+    })
+}
+
+export default meta
+type Story = StoryObj<typeof DpDetails>
+
+export const Default: Story = {
+    args: {
+        default: 'Content to be revealed onClick of the summary.',
+        summary: 'The summary tells users what to find in the details'
+    }
+}

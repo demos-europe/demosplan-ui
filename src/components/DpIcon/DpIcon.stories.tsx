c3106b9ef445689bac2d64ff2a7692c7ce2bedaa
Ahmad Alahrashi <alahrashi@demos-deutschland.de>
2023-08-01 16:43:10 +0200

    chore: Migrate DpIcon stories to v7.

---
+++ src/components/DpIcon/DpIcon.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/vue'
+import DpIcon from './DpIcon.vue'
+
+const meta: Meta<typeof DpIcon> = {
+    component: DpIcon,
+    title: "Components/Icon",
+    argTypes: {}
+}
+
+export default meta
+type Story = StoryObj<typeof DpIcon>
+
+export const Default: Story = {
+    args: {
+        icon: ''
+    }
+}

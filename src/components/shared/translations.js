2ea239530955033e4fb2f11749246cad3779f964
Heidrun Wiemold <wiemold@demos-deutschland.de>
2023-08-04 15:49:36 +0200

    refactor(refs T31187): restructure translation keys

--- src/components/shared/translations.js
+++ src/components/shared/translations.js
@@ -2,9 +2,13 @@ const de = {
   contextualHelp: 'Kontexthilfe',
   loadingData: 'Daten werden geladen...',
   selectPlaceholder: 'Bitte wählen Sie einen Eintrag aus.',
-  errorMandatoryFieldsIntro: 'Bitte korrigieren Sie Ihre Eingabe in folgenden Feldern: ',
-  errorMandatoryFieldsOutro: '. Erst dann können Sie Ihre Angaben speichern.',
-  errorMandatoryFieldsDefault: 'Bitte füllen Sie alle Pflichtfelder korrekt aus. Erst dann können Sie Ihre Angaben speichern.',
+  error: {
+    mandatoryFields: {
+      default: 'Bitte füllen Sie alle Pflichtfelder korrekt aus. Erst dann können Sie Ihre Angaben speichern.',
+      intro: 'Bitte korrigieren Sie Ihre Eingabe in folgenden Feldern: ',
+      outro: '. Erst dann können Sie Ihre Angaben speichern.'
+    }
+  }
 }
 
 export { de }

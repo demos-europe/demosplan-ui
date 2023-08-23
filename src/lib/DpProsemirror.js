09745352ac0f857603474db7435559bac4b8c76c
Florian Salis <salis@demos-deutschland.de>
2023-08-23 15:37:10 +0200

    chore: Move Prosemirror from core to here
    to avoid version missmatches. PM is very picky on this
    
    and run yarn upgrade to clean versions

---
+++ src/lib/DpProsemirror.js
@@ -0,0 +1,18 @@
+import { EditorState, TextSelection } from 'prosemirror-state'
+import { Decoration, DecorationSet, EditorView } from 'prosemirror-view'
+import { DOMParser, DOMSerializer, Schema } from 'prosemirror-model'
+import { addListNodes } from 'prosemirror-schema-list'
+import { schema } from 'prosemirror-schema-basic'
+
+export {
+  addListNodes,
+  Decoration,
+  DecorationSet,
+  DOMParser,
+  DOMSerializer,
+  EditorState,
+  EditorView,
+  Schema,
+  schema,
+  TextSelection
+}

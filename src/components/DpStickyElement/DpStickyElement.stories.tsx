1697007b52047f7a2963de9e9122aff808ad9171
Hans Spieß <spiess@demos-deutschland.de>
2023-08-31 14:50:18 +0200

    docs: pimp props controls

--- src/components/DpStickyElement/DpStickyElement.stories.tsx
+++ src/components/DpStickyElement/DpStickyElement.stories.tsx
@@ -1,6 +1,15 @@
 import type { Meta, StoryObj } from '@storybook/vue'
 import DpStickyElement from './DpStickyElement.vue'
 
+interface IDpStickyElement {
+    applyZIndex: boolean,
+    border: boolean,
+    context: HTMLElement,
+    direction: 'top' | 'bottom',
+    observeContext: boolean,
+    offset: number
+}
+
 const meta: Meta<typeof DpStickyElement> = {
     component: DpStickyElement,
     title: "Components/StickyElement",
@@ -17,11 +26,19 @@ const meta: Meta<typeof DpStickyElement> = {
     })
 }
 
-type Story = StoryObj<typeof DpStickyElement>
+type Story = StoryObj<IDpStickyElement>
 
 export default meta
 
 export const Default: Story = {
     args: {},
-    argTypes: {}
+    argTypes: {
+        direction: {
+            control: { type: 'inline-radio' },
+            options: ['top', 'bottom'],
+        },
+        offset: {
+            control: { type: 'range', min: 0, max: 500, step: 1 },
+        }
+    },
 }

c63541d7d3cf9737b4de5115f2529b21cf1e38a3
Ahmad Alahrashi <alahrashi@demos-deutschland.de>
2023-09-04 11:45:58 +0200

    chore: rebuild DpDraggable template.

--- src/components/DpDraggable/DpDraggable.stories.tsx
+++ src/components/DpDraggable/DpDraggable.stories.tsx
@@ -1,6 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/vue'
 import DpDraggable from './DpDraggable.vue'
 
+interface IDpDraggable {
+    contentData: object[]
+    'data:change': object
+}
+
 const meta: Meta<typeof DpDraggable> = {
     component: DpDraggable,
     title: "Components/Draggable",
@@ -11,20 +16,15 @@ const meta: Meta<typeof DpDraggable> = {
         setup() {
             return { args }
         },
-        template: `<dp-draggable v-bind="args">
-          <div class="o-sortablelist__item u-pv-0_5 u-pl-0_5 border--top">
-            <p>Content 1</p>
-          </div>
-          <div class="o-sortablelist__item u-pv-0_5 u-pl-0_5 border--top">
-            <p>Content 2</p>
+        template: `<dp-draggable v-model="args.contentData" v-bind="args">
+          <div
+              class="o-sortablelist__item u-pv-0_5 u-pl-0_5 border--top"
+              v-for="item in args.contentData"
+              :key="item.id">
+            {{ item.label }}
           </div>
         </dp-draggable>`,
     })
-}
-
-interface IDpDraggable {
-    contentData: string[]
-    'data:change': object
 }
 
 type Story = StoryObj<IDpDraggable>
@@ -33,7 +33,10 @@ export default meta
 
 export const Default: Story = {
     args: {
-        contentData: ['Content 1', 'Content 2']
+        contentData: [
+            { label: 'Content 1', id: '1' },
+            { label: 'Content 2', id: '2' }
+        ]
     },
     argTypes: {
         'data:change': { action: 'data:change' }

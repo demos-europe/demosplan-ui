2060803d3fe1975134225c1767220977490e9acc
Ahmad Alahrashi <59571234+ahmad-demos@users.noreply.github.com>
2023-09-05 10:05:50 +0200

    chore: add DpTooltip to Storybook. (#505)
    * chore: add DpTooltip to Storybook.
    
    - fix Tooltip container id.
    
    * chore: add CHANGELOG.md
    
    * chore: adjust text for DpTooltip.
    
    * chore: add CHANGELOG.md

---
+++ src/components/DpTooltip/DpTooltip.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/vue'
+import DpTooltip from './DpTooltip.vue'
+
+const meta: Meta<typeof DpTooltip> = {
+    component: DpTooltip,
+    title: 'Components/Tooltip',
+    render: (args) => ({
+        components: {
+            DpTooltip,
+        },
+        setup() {
+            return { args }
+        },
+        template: `<dp-tooltip v-bind='args'>
+          Tooltip Trigger.
+        </dp-tooltip>`,
+    })
+}
+
+interface IDpTooltip {
+    text: string
+}
+
+type Story = StoryObj<IDpTooltip>
+
+export default meta
+
+export const Default: Story = {
+    args: {
+        text: 'Need assistance? Click here for help and support.',
+    },
+    argTypes: { }
+}

3cd1f66fbee66f8503e1360231bdc17dd43d8c47
Ahmad Alahrashi <59571234+ahmad-demos@users.noreply.github.com>
2023-09-11 12:11:26 +0200

    Update src/components/DpSearchField/DpSearchField.stories.tsx
    Co-authored-by: spiess-demos <40452344+spiess-demos@users.noreply.github.com>

--- src/components/DpSearchField/DpSearchField.stories.tsx
+++ src/components/DpSearchField/DpSearchField.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/vue'
-import DpSearchField from './DpSearchField.vue'
+import DpSearchField from './'
 
 const meta: Meta<typeof DpSearchField> = {
     component: DpSearchField,

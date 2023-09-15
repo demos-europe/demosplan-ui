835de30d72eb741722ed34a29ac169dd62c708df
Ahmad Alahrashi <59571234+ahmad-demos@users.noreply.github.com>
2023-09-15 10:31:59 +0200

    chore: add DpTimePicker to Storybook. (#525)
    * chore: add DpTimePicker to Storybook.
    
    * chore: add CHANGELOG
    
    * Update src/components/DpTimePicker/DpTimePicker.stories.mdx
    
    Co-authored-by: spiess-demos <40452344+spiess-demos@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: spiess-demos <40452344+spiess-demos@users.noreply.github.com>

---
+++ src/components/DpTimePicker/DpTimePicker.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/vue'
+import DpTimePicker from './'
+
+const meta: Meta<typeof DpTimePicker> = {
+    component: DpTimePicker,
+    title: "Components/TimePicker",
+
+}
+
+interface IDpTimePicker {
+    id: string
+    input: object
+}
+
+type Story = StoryObj<IDpTimePicker>
+
+export default meta
+
+export const Default: Story = {
+    args: {
+        id: 'TimePicker_id'
+    },
+    argTypes: {
+        input: { action: 'input' },
+    }
+}

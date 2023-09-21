a18a09fddf0bd677e6587b53fd9215375c9bc824
Ahmad Alahrashi <59571234+ahmad-demos@users.noreply.github.com>
2023-09-21 13:32:50 +0200

    chore: WIP: Add DpModal to Storybook (#538)
    * chore: WIP: Add DpModal to Storybook
    
    * chore: Fix getComputedStyle error, adjust DpModal Storybook.
    
    * Update src/components/DpModal/DpModal.stories.mdx
    
    Co-authored-by: spiess-demos <40452344+spiess-demos@users.noreply.github.com>
    
    * Update src/components/DpModal/DpModal.stories.mdx
    
    Co-authored-by: spiess-demos <40452344+spiess-demos@users.noreply.github.com>
    
    * Update src/components/DpModal/DpModal.stories.mdx
    
    Co-authored-by: spiess-demos <40452344+spiess-demos@users.noreply.github.com>
    
    * chore: adjust document.
    
    * Update src/components/DpModal/DpModal.stories.tsx
    
    Co-authored-by: spiess-demos <40452344+spiess-demos@users.noreply.github.com>
    
    * Update src/components/DpModal/DpModal.stories.tsx
    
    Co-authored-by: spiess-demos <40452344+spiess-demos@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: spiess-demos <40452344+spiess-demos@users.noreply.github.com>

---
+++ src/components/DpModal/DpModal.stories.tsx
@@ -0,0 +1,54 @@
+import type { Meta, StoryObj } from '@storybook/vue'
+import DpModal from './'
+
+const meta: Meta<typeof DpModal> = {
+    component: DpModal,
+    title: 'Components/Modal',
+    render: (args) => ({
+        components: {
+            DpModal
+        },
+        setup() {
+            return { args }
+        },
+        template: `
+          <div>
+            <a
+                href="#openModal"
+                id="openModal"
+                @click.stop.prevent="$refs.openModal.toggle()"
+                role="button">
+              Open Modal
+            </a>
+            <dp-modal
+                id="openModal"
+                ref="openModal"
+                v-bind="args">
+              <template v-slot:header>
+                Short, descriptive modal title
+              </template>
+              <template>
+                <p>
+                  Modal content
+                </p>
+              </template>
+            </dp-modal>
+          </div>
+        `,
+    })
+}
+
+interface IDpModal {
+    'modal:toggled': object
+}
+
+type Story = StoryObj<IDpModal>
+
+export default meta
+
+export const Default: Story = {
+    args: {},
+    argTypes: {
+        'modal:toggled': { action: 'modal:toggled' }
+    }
+}

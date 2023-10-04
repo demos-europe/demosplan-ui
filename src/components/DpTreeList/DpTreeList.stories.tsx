eed6731471ed4e5a85df9c77394028248f78c0b2
Ahmad Alahrashi <alahrashi@demos-deutschland.de>
2023-10-04 11:29:47 +0200

    chore: WIP: Add DpTreeList to Storybook.

--- src/components/DpTreeList/DpTreeList.stories.tsx
+++ src/components/DpTreeList/DpTreeList.stories.tsx
@@ -4,6 +4,42 @@ import DpTreeList from './'
 const meta: Meta<typeof DpTreeList> = {
     component: DpTreeList,
     title: 'Components/TreeList',
+    render: (args) => ({
+        components: {
+            DpTreeList
+        },
+        setup() {
+            return { args }
+        },
+        template: `<dp-tree-list v-bind="args">
+          <template v-slot:header>
+            <div class="layout--flush">
+              <div class="layout__item u-5-of-12">
+                Heading
+              </div>
+              <div class="layout__item u-5-of-12">
+                Status
+              </div>
+            </div>
+          </template>
+          <template v-slot:branch="{ nodeElement }">
+            {{ nodeElement.attributes.title }}
+          </template>
+          <template v-slot:leaf="{ nodeElement }">
+            {{ nodeElement.children.attributes.title }}
+          </template>
+          <template v-slot:footer>
+            <button
+                type="submit"
+                class="btn btn--primary">
+              <i
+                  class="fa fa-download u-mr-0_25"
+                  aria-hidden="true" />
+              Button-Label
+            </button>
+          </template>
+        </dp-tree-list>`,
+    })
 }
 
 interface IDpTreeList {
@@ -11,7 +47,7 @@ interface IDpTreeList {
     'draggable:change': object
     nodeSelectionChange: object
     branchIdentifier: Function
-    treeData: string[]
+    treeData: object[]
 }
 
 type Story = StoryObj<IDpTreeList>
@@ -21,7 +57,54 @@ export default meta
 export const Default: Story = {
     args: {
         branchIdentifier: () => '',
-        treeData: ['option 1', 'option 2']
+        treeData: [
+            {
+                attributes: {
+                    title: 'Category 1'
+                },
+                children: [
+                    {
+                        attributes: {
+                            title: 'Sub-Category 1'
+                        },
+                        id: 'id-attributes-1'
+                    }
+                ],
+                id: 'id-1',
+                relationships: {
+                    faq: {
+                        data: [{
+                            id: 'id-1',
+                            type: 'faq'
+                        }]
+                    },
+                    type: 'faqCategory'
+                }
+            },
+            {
+                attributes: {
+                    title: 'Category 2'
+                },
+                children: [
+                    {
+                        attributes: {
+                            title: 'Sub-Category 2'
+                        },
+                        id: 'id-attributes-2'
+                    }
+                ],
+                id: 'id-2',
+                relationships: {
+                    faq: {
+                        data: [{
+                            id: 'id-2',
+                            type: 'faq'
+                        }]
+                    },
+                    type: 'faqCategory'
+                }
+            }
+        ]
     },
     argTypes: {
         'tree:change': { action: 'tree:change' },

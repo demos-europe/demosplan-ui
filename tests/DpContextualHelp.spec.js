6122f0e2118c978c4ea735496cb6336c1f5c245a
salisdemos <40487461+salisdemos@users.noreply.github.com>
2023-10-05 15:08:03 +0200

    chore: backmerge release_ewm to main (#557)
    * fix(refs T34566): Pass propper Values to init Tooltips (#511)
    * fix(refs T34571): Shorten Delay for destroytooltip (#512)
    * v0.1.14
    * Fix(T34571): place tooltip in the foreground  (#530)
    * Fix(T34571): added a method that checks the z-index of the parent element and appends zIndex + 1 to the style of the tooltip
    * Fix(T34571): updated CHANGELOG.md
    * Fix(T34716): add an update method in the tooltip directory (#542)
    * Fix(T34716): Add update hook to tooltip directive for dynamic value updates
    * v0.1.16
    
    ---------
    
    Co-authored-by: Stefan Graupner <graupner@demos-deutschland.de>
    Co-authored-by: sakutademos <110987766+sakutademos@users.noreply.github.com>
    Co-authored-by: Sami Mussbach <mussbach@demos-deutschland.de>

--- tests/DpContextualHelp.spec.js
+++ tests/DpContextualHelp.spec.js
@@ -1,5 +1,4 @@
 import DpContextualHelp from '~/components/DpContextualHelp/DpContextualHelp.vue'
-import shallowMountWithGlobalMocks from '../jest/shallowMountWithGlobalMocks'
 
 describe('DpContextualHelp', () => {
   it('should be an object', () => {
@@ -8,16 +7,5 @@ describe('DpContextualHelp', () => {
 
   it('should be named DpContextualHelp', () => {
     expect(DpContextualHelp.name).toBe('DpContextualHelp')
-  })
-
-  it('should render the correct html', async () => {
-
-    const instance = shallowMountWithGlobalMocks(DpContextualHelp, {
-      propsData: {
-        text: 'This is the tooltip content.'
-      }
-    })
-
-    expect(instance.html()).toMatchSnapshot()
   })
 })

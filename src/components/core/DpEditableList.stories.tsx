b5f89149b3a087b6e06e24fff991a5b3da898b66
Ahmad Alahrashi <59571234+ahmad-demos@users.noreply.github.com>
2023-11-10 12:18:54 +0100

    F t34965 add methods für dp editable storybook (#628)
    * chore (refs T34965): WIP: Add methods for DpEditableList Storybook.
    
    - Add data, methods.
    
    * chore (refs T34965): rename data & methods.
    
    * chore (refs T34965): add showUpdateForm event.

--- src/components/core/DpEditableList.stories.tsx
+++ src/components/core/DpEditableList.stories.tsx
@@ -8,27 +8,95 @@ const meta: Meta<typeof DpEditableList> = {
         components: {
             DpEditableList
         },
+        data () {
+            return {
+                formFields: {
+                    item: ''
+                },
+                entries: args.entries,
+                itemIndex: null
+            }
+        },
         setup() {
             return { args }
         },
+        methods: {
+            addItem () {
+                this.entries.push({
+                    item: this.formFields.item
+                })
+            },
+
+            deleteItem (index) {
+                this.entries.splice(index, 1)
+                this.updateExtraItems(index)
+            },
+
+            handleSubmit (index) {
+                if (index === 'new') {
+                    this.addItem()
+                    this.saveExtraItems(this.formFields.item)
+                } else {
+                    this.updateItems(index)
+                    this.updateExtraItems(index, this.formFields.item[index])
+                }
+
+                this.resetForm()
+                this.$refs.listComponent.toggleFormVisibility(false)
+                this.$refs.listComponent.currentlyUpdating = ''
+            },
+
+            resetForm () {
+                this.formFields.item = ''
+                this.itemIndex = null
+            },
+
+            saveExtraItems (extraItems) {
+                this.$emit('saved', extraItems)
+            },
+
+            updateExtraItems (index, extraItems) {
+                this.$emit('updated', (index, extraItems))
+            },
+
+            updateItems (index) {
+                this.entries[index].item = this.formFields.item
+            },
+        },
+        mounted () {
+            this.$on('delete', (index) => {
+                this.deleteItem(index)
+                this.resetForm()
+            })
+
+            this.$on('showUpdateForm', (index) => {
+                this.formFields.item = this.entries[index].item
+                this.itemIndex = index
+            })
+        },
         template: `
           <dp-editable-list
-              :entries="args.entries"
-              v-bind="args">
+              :entries="entries"
+              v-bind="args"
+              @reset="resetForm"
+              @saveEntry="handleSubmit(itemIndex !== null ? itemIndex : 'new')"
+              ref="listComponent">
             <template v-slot:list="entry">
                 <span>
-                  {{ entry.mail }}
+                  {{ entry.item }}
                   <input
                       class="hide-visually"
                       type="email"
-                      :value="entry.mail">
+                      :value="entry.item">
                 </span>
             </template>
             <template v-slot:form="entry">
               <input
                   id="emailAddress"
                   type="email"
-                  v-model="args.entries.mail"/>
+                  placeholder="Email Address"
+                  v-model="formFields.item"
+                  @enter="handleSubmit(itemIndex !== null ? itemIndex : 'new')" />
             </template>
           </dp-editable-list>
         `,
@@ -37,8 +105,10 @@ const meta: Meta<typeof DpEditableList> = {
 
 interface IDpEditableList {
     entries: object[]
+    delete: object
     reset: object
     saveEntry: object
+    showUpdateForm: object
 }
 
 type Story = StoryObj<IDpEditableList>
@@ -49,15 +119,17 @@ export const Default: Story = {
     args: {
         entries: [
             {
-                mail: "mail1@mail.com"
+                item: "mail1@mail.com"
             },
             {
-                mail: "mail2@mail.com"
+                item: "mail2@mail.com"
             }
         ],
     },
     argTypes: {
+        delete: { action: 'delete' },
         reset: { action: 'reset' },
-        saveEntry: { action: 'saveEntry' }
+        saveEntry: { action: 'saveEntry' },
+        showUpdateForm: { action: 'showUpdateForm' }
     }
 }

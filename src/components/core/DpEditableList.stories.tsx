0bcf8e32fd29c5e726d4f3573ca4054d491e6238
Florian Salis <salis@demos-deutschland.de>
2023-12-05 15:58:31 +0100

    chore: Simplify editableList story.
    The old way broke the build doue to errors.
    
    This may not be perfect, but works for now and can easily be extended.

--- src/components/core/DpEditableList.stories.tsx
+++ src/components/core/DpEditableList.stories.tsx
@@ -8,103 +8,27 @@ const meta: Meta<typeof DpEditableList> = {
         components: {
             DpEditableList
         },
-        data () {
-            return {
-                formFields: {
-                    item: ''
-                },
-                entries: args.entries,
-                itemIndex: null
-            }
-        },
         setup() {
             return { args }
         },
-        methods: {
-            addItem () {
-                this.entries.push({
-                    item: this.formFields.item
-                })
-            },
-
-            deleteItem (index) {
-                this.entries.splice(index, 1)
-                this.updateExtraItems(index)
-            },
-
-            handleSubmit (index) {
-                if (index === 'new') {
-                    this.addItem()
-                    this.saveExtraItems(this.formFields.item)
-                } else {
-                    this.updateItems(index)
-                    this.updateExtraItems(index, this.formFields.item[index])
-                }
-
-                this.resetForm()
-                this.$refs.listComponent.toggleFormVisibility(false)
-                this.$refs.listComponent.currentlyUpdating = ''
-            },
-
-            resetForm () {
-                this.formFields.item = ''
-                this.itemIndex = null
-            },
-
-            saveExtraItems (extraItems) {
-                this.$emit('saved', extraItems)
-            },
-
-            updateExtraItems (index, extraItems) {
-                this.$emit('updated', (index, extraItems))
-            },
-
-            updateItems (index) {
-                this.entries[index].item = this.formFields.item
-            },
-        },
-        mounted () {
-            this.$on('delete', (index) => {
-                this.deleteItem(index)
-                this.resetForm()
-            })
-
-            this.$on('showUpdateForm', (index) => {
-                this.formFields.item = this.entries[index].item
-                this.itemIndex = index
-            })
-        },
         template: `
-          <dp-editable-list
-              :entries="entries"
-              v-bind="args"
-              @reset="resetForm"
-              @saveEntry="handleSubmit(itemIndex !== null ? itemIndex : 'new')"
-              ref="listComponent">
-            <template v-slot:list="entry">
-                <span>
-                  {{ entry.item }}
-                  <input
-                      class="hide-visually"
-                      type="email"
-                      :value="entry.item">
-                </span>
-            </template>
-            <template v-slot:form="entry">
-              <input
-                  id="emailAddress"
-                  type="email"
-                  placeholder="Email Address"
-                  v-model="formFields.item"
-                  @enter="handleSubmit(itemIndex !== null ? itemIndex : 'new')" />
-            </template>
-          </dp-editable-list>
-        `,
+            <dp-editable-list v-bind="args">
+                <template v-slot:list="{ entry }">
+                  <span>{{ entry.mail }}
+                      <input
+                        type="email"
+                        :value="entry.mail"
+                        name="formFieldName">
+                  </span>
+                </template>
+            </dp-editable-list>
+        `
     })
 }
 
 interface IDpEditableList {
-    entries: object[]
+    entries: object[] | object
+    index: number[],
     delete: object
     reset: object
     saveEntry: object
@@ -118,13 +42,10 @@ export default meta
 export const Default: Story = {
     args: {
         entries: [
-            {
-                item: "mail1@mail.com"
-            },
-            {
-                item: "mail2@mail.com"
-            }
+            { mail: 'mail1@mail.com' },
+            { mail: 'mail2@mail.com' }
         ],
+        index: [0, 1]
     },
     argTypes: {
         delete: { action: 'delete' },

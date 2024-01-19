4c4a4367f70ae2910cf4e87162bc78af53c4590c
Lamia Gülsah Elmas <elmas@demos-deutschland.de>
2024-01-19 18:12:58 +0100

    fix(refs:T35629): fix tooltip position

--- src/components/DpLabel/DpLabel.vue
+++ src/components/DpLabel/DpLabel.vue
@@ -16,7 +16,7 @@
     </span>
     <i
       v-if="tooltip !== ''"
-      :class="prefixClass('fa fa-question-circle u-mt-0_125 ml-auto')"
+      :class="prefixClass('fa fa-question-circle u-mt-0_125')"
       :aria-label="ariaLabel"
       v-tooltip="tooltip" />
   </label>

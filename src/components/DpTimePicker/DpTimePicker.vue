c6f5e7a1995248d2395d20ddbf63154954474229
Ahmad Alahrashi <59571234+ahmad-demos@users.noreply.github.com>
2024-05-02 12:50:16 +0200

    Update src/components/DpTimePicker/DpTimePicker.vue
    Co-authored-by: hwiem <40487668+hwiem@users.noreply.github.com>

--- src/components/DpTimePicker/DpTimePicker.vue
+++ src/components/DpTimePicker/DpTimePicker.vue
@@ -31,7 +31,7 @@
       type="time"
       pattern="([01]?[0-9]|2[0-3]):[0-5][0-9]"
       :model-value="currentTime"
-      @update:model-value="val => handleInput(val)"
+      @update:modelValue="val => handleInput(val)"
       autocomplete="off" />
 
     <div

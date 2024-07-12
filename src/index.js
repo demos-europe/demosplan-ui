3a1caddd614cc71f822d527758c15854572dbb59
Florian Salis <salis@demos-deutschland.de>
2024-07-12 09:02:01 +0200

    chore: fix merge issues

--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
-import * as shared from './shared'
-import * as directives from './directives'
-import * as utils from './utils'
-import * as components from './components/'
-import * as mixins from './mixins/'
-import * as lib from './lib/'
-import * as validation from './lib/validation/'
-
-export * from './shared'
+export * from './components'
 export * from './directives'
+export * from './lib'
+export * from './mixins'
+export * from './shared'
 export * from './utils'
-export * from './components/'
-export * from './mixins/'
-export * from './lib/'
-export * from './lib/validation/'
+export * from './lib/validation'
 
+import * as components from './components'
+import * as directives from './directives'
+import * as lib from './lib'
+import * as mixins from './mixins'
+import * as shared from './shared'
+import * as utils from './utils'
+import * as validation from './lib/validation'
 
 export default {
-  ...shared,
-  ...directives,
-  ...utils,
   ...components,
-  ...mixins,
+  ...directives,
   ...lib,
+  ...mixins,
+  ...shared,
+  ...utils,
   ...validation
 }

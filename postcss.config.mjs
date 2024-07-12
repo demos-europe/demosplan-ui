7589b5342571ec488e71ea874784f46a3fb1d280
Florian Salis <salis@demos-deutschland.de>
2024-07-12 09:03:33 +0200

    chore: fix merge issues

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,9 +2,6 @@ export default function postcssConfig () {
   // Add you postcss configuration here
   // Learn more about it at https://github.com/webpack-contrib/postcss-loader#config-files
   return {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {}
-    }
+    plugins: [["tailwindcss"], ["autoprefixer"]]
   }
 }

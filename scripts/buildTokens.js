086b57ea1a8c530937c369ce4b187e3e97ab68f1
Hans Spieß <spiess@demos-deutschland.de>
2024-07-19 18:11:26 +0200

    feat: WIP refactor style dictionary script

---
+++ scripts/buildTokens.js
@@ -0,0 +1,118 @@
+const glob = require('glob')
+const StyleDictionary = require('style-dictionary')
+const {
+  transformScssTokens,
+  transformTailwindTokens
+} = require('./utils/transformTokens')
+
+const prefix = 'dp-'
+
+const tokensPath = 'tokens/src/**/*.json'
+
+const files = glob
+  .sync(tokensPath)
+  .map(filePath => filePath
+    .replace('tokens/src/', '')
+    .replace('color/', '')
+    .replace('.json', ''))
+    .filter(filePath => !filePath.startsWith('_')) // Do not render tokens only used internally
+
+// Define custom output groups for Tailwind, rather than just grouping them by file path
+const corePlugins = ['fontSize', 'boxShadow', 'breakpoints', 'rounded', 'space', 'zIndex']
+const corePluginsColor = ['color', 'textColor', 'backgroundColor', 'borderColor']
+
+// Adds prefix to variables. Keeps uppercase path parts, which is needed later.
+StyleDictionary.registerTransform({
+  name: 'name/scss',
+  type: 'name',
+  transformer: (token) => prefix + token.path.join('-')
+})
+
+// Adds prefix transform to default scss transform group
+StyleDictionary.registerTransformGroup({
+  name: 'custom/scss',
+  transforms: StyleDictionary.transformGroup.scss.concat(['name/scss'])
+})
+
+// Adds prefix to variables. Keeps uppercase path parts, which is needed later.
+StyleDictionary.registerTransform({
+  name: 'name/web',
+  type: 'name',
+  transformer: (token) => token.path.join('-')
+})
+
+// Adds prefix transform to default scss transform group
+StyleDictionary.registerTransformGroup({
+  name: 'custom/web',
+  transforms: StyleDictionary.transformGroup.web.concat(['name/web'])
+})
+
+/**
+ * Custom format that generates javascript modules ready to be used as Tailwind config files.
+ * The values use a nested set of css variables to enable theming on multiple levels.
+ */
+StyleDictionary.registerFormat({
+  name: 'tailwind/variables',
+  formatter: (formatterArguments) => transformTailwindTokens(formatterArguments, corePluginsColor, corePlugins)
+})
+
+/**
+ * This format is a customized version of the implementation in `formattedVariables`.
+ * It applies some transformations to scss variables according to the conventions in demosplan-ui.
+ * See https://github.com/amzn/style-dictionary/blob/17f4cb2f30bd002dfd55d6ef8c5bee4138de8d64/lib/common/formatHelpers/formattedVariables.js#L46
+ */
+StyleDictionary.registerFormat({
+  name: 'scss/customVariables',
+  formatter: transformScssTokens
+})
+
+const StyleDictionaryExtended = StyleDictionary.extend({
+  source: [tokensPath],
+  platforms: {
+    js: {
+      transformGroup: 'js',
+      buildPath: 'tokens/dist/js/',
+      files: files.map((filePath) => {
+        return {
+          destination: `${filePath}.js`,
+          format: 'javascript/module',
+          filter: (token) => {
+            return token.filePath.includes(filePath) && token.$status !== 'Deprecated'
+          },
+          options: {
+            outputReferences: true
+          }
+        }
+      })
+    },
+    scss: {
+      transformGroup: 'custom/scss',
+      prefix,
+      buildPath: 'tokens/dist/scss/',
+      files: files
+        .map((filePath) => {
+          return {
+            destination: `_${filePath}.scss`,
+            format: 'scss/customVariables',
+            filter: (token) => token.filePath.includes(filePath),
+            options: {
+              outputReferences: true
+            }
+          }
+        })
+    },
+    web: {
+      transformGroup: 'custom/web',
+      prefix,
+      buildPath: 'tokens/dist/tailwind/',
+      files: [...corePlugins, ...corePluginsColor].map((plugin) => {
+        return {
+          destination: `${plugin}.js`,
+          format: 'tailwind/variables'
+        }
+      }),
+    },
+  }
+})
+
+StyleDictionaryExtended.buildAllPlatforms()
---
+++ scripts/utils/transformTokens.js
@@ -0,0 +1,247 @@
+const StyleDictionary = require("style-dictionary");
+const {
+  createPropertyFormatter,
+  fileHeader,
+  sortByReference
+} = StyleDictionary.formatHelpers
+
+// Resolve token values with fallback, for css variables nesting
+const resolveValue = (token, dictionary) => {
+  let value = token.value
+  const seen = new Set()
+
+  while (dictionary.usesReference(value)) {
+    seen.add(value)
+    value = dictionary.getReferences(value)[0].value
+    if (seen.has(value)) break
+  }
+
+  return value
+}
+
+// Turn a dash-case-string into a lowerCamelString
+const dashToLowerCamel = (str) => {
+  return str.replace(/-([a-z])/g, function (g) {
+    return g[1].toUpperCase()
+  })
+}
+
+// Turn a lowerCamelString into a dash-case-string
+const lowerCamelToDash = (str) => {
+  return str.replace(/([A-Z])/g, '-$1').toLowerCase()
+}
+
+// Build Css variables chain to resolve aliases
+const transformTailwindTokenValue = (token, formatterArguments) => {
+  const { dictionary, platform } = formatterArguments
+  const varName = `--${platform.prefix}${transformTailwindTokenName(token, true).replace(/\./g, '-')}`
+  let fallback = resolveValue(token, dictionary)
+
+  let current = token
+  let cssVar = `var(${varName}`
+  while (current.original && current.original.value.startsWith('{')) {
+    const ref = dictionary.getReferences(current.original.value)[0]
+    let refName = transformTailwindTokenName(ref, true)
+    refName = `--${platform.prefix}${refName.replace(/\./g, '-')}`
+    cssVar += `, var(${refName}`
+
+    current = ref
+  }
+
+  cssVar += `, ${fallback})`.repeat(cssVar.match(/var\(/g).length)
+
+  let regex = /#([0-9a-fA-F]{6})\), #([0-9a-fA-F]{6})/
+  return cssVar.replace(regex, '#$1)')
+}
+
+const transformTailwindTokensGrouped = (formatterArguments, corePluginsColor) => {
+  const { dictionary, file, platform } = formatterArguments
+  const groupedTokens = {}
+
+  corePluginsColor.forEach(plugin => {
+    groupedTokens[plugin] = {}
+  })
+
+  dictionary.allTokens.forEach(token => {
+    // Do not include legacy ui and palette colors
+    if (token.path[0] !== 'color' || token.path[0] === 'color' && ['ui', 'palette'].includes(token.path[1])) {
+      return false
+    }
+
+    const tokenName = transformTailwindTokenName(token)
+    const replaced = transformTailwindTokenValue(token, formatterArguments)
+
+    // Sort token definition into respective group
+    corePluginsColor.forEach(plugin => {
+      if (token.path[2] === plugin || ['brand', 'data'].includes(token.path[1]) && plugin === 'color') {
+        groupedTokens[plugin][tokenName] = replaced + ';'
+      }
+    })
+  })
+
+  return `module.exports = ${JSON.stringify(groupedTokens[file.destination.replace('.js', '')], null, 2)};`
+}
+
+const isDestinationToken = (file, token) => {
+  const filePart = file.destination.replace('.js', '')
+  const dashedTokenPath = dashToLowerCamel(token.path[0])
+  return dashedTokenPath === filePart
+}
+
+const transformTailwindTokensFlat = (formatterArguments) => {
+  const { dictionary, file, platform } = formatterArguments
+  const tokens = {}
+
+  dictionary.allTokens.forEach(token => {
+    if (!isDestinationToken(file, token) || token['$status'] === 'Deprecated' || token.path[0] === 'fontSize' && token.path[1] === 'scale') {
+      return
+    }
+
+    const tokenName = transformTailwindTokenName(token)
+    const tokenValue = transformTailwindTokenValue(token, formatterArguments)
+
+    tokens[tokenName] = tokenValue + ';'
+  })
+
+  return `module.exports = ${JSON.stringify(tokens, null, 2)};`
+}
+
+// Extract variable name from Scss variables declaration
+const getScssVariableName = (declaration) => {
+  const regex = /\$([\w.-]+)\s*:/
+  const match = declaration.match(regex)
+
+  if (match) {
+    return match[1]
+  }
+
+  return null
+}
+
+// Replace string in Scss variable name via regex
+const replaceScssVariableName = (declaration, searchRegex, replaceStr) => {
+  const variableName = getScssVariableName(declaration)
+  if (variableName) {
+    const updatedVariableName = variableName.replace(searchRegex, replaceStr)
+    return declaration.replace(variableName, updatedVariableName)
+  }
+
+  return declaration
+}
+
+// Transforms that apply to both Scss and Tailwind platforms
+const transformDeclarationDefault = (declaration, tokenPath) => {
+  // The domain part within font-size tokens should not be part of the variable name.
+  if (tokenPath[0] === 'fontSize') {
+    declaration = declaration.replace(/-(scale|brand|heading|ui)/g, '')
+  }
+
+  // The domain part within color tokens should not be part of the variable name.
+  if (tokenPath[0] === 'color') {
+    declaration = declaration.replace(/-(brand|data|palette|ui-tailwind|ui)/g, '')
+  }
+
+  return declaration
+}
+
+// Transforms that only apply to Tailwind token name
+const transformTailwindTokenName = (token, keepPluginName = false) => {
+  let declaration = token.name
+  const tokenPath = token.path
+
+  declaration = transformDeclarationDefault(declaration, tokenPath)
+
+  // Within css variable declarations, we want to keep plugin names
+  if (!keepPluginName) {
+    if (tokenPath[0] === 'color' || tokenPath[1] === 'ui-tailwind') {
+      declaration = declaration.replace(/color-/g, '')
+    }
+
+    if (tokenPath[0] === 'color' && tokenPath[1] === 'ui-tailwind') {
+      declaration = declaration.replace(/(textColor|backgroundColor|borderColor)-/g, '')
+    }
+
+    if (['boxShadow', 'breakpoints', 'fontSize', 'rounded', 'space', 'zIndex'].includes(tokenPath[0])) {
+      declaration = declaration.replace(/(boxShadow|breakpoints|fontSize|rounded|space|zIndex)?-/g, '')
+    }
+  }
+
+  // Handle special cases with css var naming
+  if (tokenPath[1] === 'ui-tailwind' && tokenPath[2] !== 'color') {
+    declaration = declaration.replace(/color-/g, '')
+  } else if (tokenPath[1] === 'ui-tailwind' && tokenPath[2] === 'color') {
+    declaration = declaration.replace(/color-color-/g, 'color-')
+  }
+
+  // "DEFAULT" is a Tailwind convention that should not be part of the declaration key.
+  declaration = declaration.replace(/-DEFAULT/g, '')
+  if (token.path[0] === 'zIndex') {
+    console.log(declaration)
+  }
+  return declaration
+}
+
+// Transforms that only apply to Scss platform
+const transformDeclarationScss = (declaration, tokenPath) => {
+  declaration = transformDeclarationDefault(declaration, tokenPath)
+
+  // The key "z-index" should be shortened in the variable name to match Tailwind syntax
+  if (tokenPath[0] === 'zIndex') {
+    declaration = declaration.replace(/(z-index|zIndex)-/g, 'z-')
+  }
+
+  // Scss does not like variable names with dots in them.
+  declaration = replaceScssVariableName(declaration, /\./g, '_')
+
+  // Where possible, the lowerCamel case is used for naming in token sources. In Scss, dash-case is the convention.
+  declaration = declaration.replace(/(fontSize|boxShadow|zIndex)/g, lowerCamelToDash)
+
+  // "DEFAULT" is a Tailwind convention that should not be part of the Scss name.
+  declaration = declaration.replace(/-DEFAULT/g, '')
+
+  return declaration
+}
+
+const transformScssTokens = ({dictionary, options, file}) => {
+  const { outputReferences, themeable = false, formatting } = options
+
+  let { allTokens } = dictionary
+
+  if (outputReferences) {
+    allTokens = [...allTokens].sort(sortByReference(dictionary))
+  }
+
+  const propertyFormatter = createPropertyFormatter({ outputReferences, dictionary, format: 'sass', formatting })
+
+  const tokens = allTokens
+    .map(token => {
+      /**
+       * Within color.ui.json, legacy tokens for scss and tailwind tokens are separated into two top-tier
+       * properties "ui" and "ui-tailwind". For Scss, only the tokens in the "ui" group should be rendered.
+       */
+      if (token.path[1] ===  'ui-tailwind') {
+        return
+      }
+
+      // First, the token is formatted with the default Scss propertyFormatter
+      const formattedTokenDeclaration = propertyFormatter(token)
+
+      // Then, our specific transforms are applied to each line.
+      return transformDeclarationScss(formattedTokenDeclaration, token.path)
+    })
+    .filter(function(strVal) { return !!strVal })
+    .join('\n')
+
+  return fileHeader({file, commentStyle: 'short'}) + '\n' + tokens + '\n'
+}
+
+const transformTailwindTokens =  (formatterArguments, corePluginsColor) => {
+  const { file } = formatterArguments
+  const isColorTokenFile = corePluginsColor.some(plugin => file.destination.startsWith(plugin))
+
+  return isColorTokenFile
+    ? transformTailwindTokensGrouped(formatterArguments, corePluginsColor)
+    : transformTailwindTokensFlat(formatterArguments)
+}
+
+module.exports = { transformScssTokens, transformTailwindTokens }

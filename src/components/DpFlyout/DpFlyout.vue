9a337fd766f94b18f3bfad1c2619ffa895486d80
Ahmad Alahrashi <email-vom-gpg-key>
2024-09-23 12:44:28 +0200

    chor (refs DPLAN-12460): WIP: Add floating-ui to flyout component.

--- src/components/DpFlyout/DpFlyout.vue
+++ src/components/DpFlyout/DpFlyout.vue
@@ -8,8 +8,8 @@
       'is-expanded': isExpanded,
       'o-flyout--menu': hasMenu
     }"
-    v-click-outside="close"
-    data-cy="flyoutTrigger">
+    data-cy="flyoutTrigger"
+    ref="reference">
     <button
       :disabled="disabled"
       type="button"
@@ -25,6 +25,7 @@
     </button>
     <div
       class="o-flyout__content shadow"
+      ref="flyout"
       data-cy="flyout">
       <slot />
     </div>
@@ -32,14 +33,10 @@
 </template>
 
 <script>
-import ClickOutside from 'vue-click-outside'
+import { computePosition, offset, flip, shift, autoUpdate } from '@floating-ui/dom'
 
 export default {
   name: 'DpFlyout',
-
-  directives: {
-    ClickOutside
-  },
 
   props: {
     align: {
@@ -76,7 +73,8 @@ export default {
 
   data () {
     return {
-      isExpanded: false
+      isExpanded: false,
+      cleanup: null
     }
   },
 
@@ -85,6 +83,8 @@ export default {
       if (this.isExpanded === true) {
         this.$emit('close')
         this.isExpanded = false
+        this.cleanup?.()
+        document.removeEventListener('click', this.handleOutsideClick)
       }
     },
 
@@ -93,14 +93,60 @@ export default {
 
       if (this.isExpanded) {
         this.$emit('open')
+        const insertedEL = document.body.appendChild(this.$refs.flyout)
+        this.updatePosition(insertedEL)
+        // this.cleanup = autoUpdate(this.$refs.reference, this.$refs.flyout, this.updatePosition)
+        document.addEventListener('click', this.handleOutsideClick)
       } else {
         this.$emit('close')
+        this.$refs.flyout.remove()
+        this.cleanup?.()
+        document.removeEventListener('click', this.handleOutsideClick)
+      }
+    },
+
+    async updatePosition (el) {
+      this.cleanup = await computePosition(this.$refs.reference, el, {
+        placement: this.align === 'left' ? 'bottom-start' : 'bottom-end',
+        middleware: [
+          offset(10),
+          flip({
+            fallbackPlacements: ['top-start', 'top-end'],
+            rootBoundary: 'body'
+          }),
+          shift({
+            padding: 8,
+            rootBoundary: 'body'
+          })
+        ],
+      }).then(({x, y}) => {
+        console.log(x, y)
+        Object.assign(el.style, {
+          left: `${x}px`,
+          top: `${y}px`,
+          zIndex: '999999',
+          display: 'block'
+        })
+      })
+    },
+
+    handleOutsideClick (event) {
+      if (!this.$el.contains(event.target)) {
+        this.close()
+        this.$refs.flyout.remove()
       }
     }
   },
 
   mounted () {
-    this.popupItem = this.$el
+    if (this.isExpanded) {
+      this.updatePosition(this.$refs.flyout)
+    }
+  },
+
+  beforeDestroy () {
+    this.cleanup?.()
+    document.removeEventListener('click', this.handleOutsideClick)
   }
 }
 </script>

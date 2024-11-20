f28fa7ae82d4ef5e4bc61585ad46f9c59249fb99
Heidrun Wiemold <wiemold@demos-deutschland.de>
2024-11-20 09:44:20 +0100

    style(refs DPLAN-12907): make spacing consistent

--- src/components/DpEditor/DpUploadModal.vue
+++ src/components/DpEditor/DpUploadModal.vue
@@ -11,10 +11,12 @@
     </h3>
     <h3
       v-else
-      class="u-mb">
+      class="mb-2">
       {{ translations.insertImage }}
     </h3>
-    <div v-if="!editAltTextOnly">
+    <div
+      v-if="!editAltTextOnly"
+      class="mb-2">
       <dp-upload-files
         allowed-file-types="img"
         :basic-auth="basicAuth"

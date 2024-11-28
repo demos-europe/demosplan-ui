b17ba87465e849eb1c134d8b67f73ba9090ff1de
Florian Salis <salis@demos-deutschland.de>
2024-11-28 11:44:54 +0100

    chore: add comment

--- src/components/DpEditor/libs/editorMention.js
+++ src/components/DpEditor/libs/editorMention.js
@@ -1,4 +1,6 @@
 import { mergeAttributes } from '@tiptap/core'
+// THIS IS A WORKAROUND.
+// In our setup per default the cjs-file is used. That file for some reason has a problem to resolve an internal dependency (Suggestion) properly.
 import { Mention } from '../../../../node_modules/@tiptap/extension-mention/dist/index.js'
 
 export default Mention.extend({

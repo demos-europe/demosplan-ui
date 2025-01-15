df50704a2ed7af1be087465640bb82627b312c9a
Heidrun Wiemold <wiemold@demos-deutschland.de>
2025-01-15 12:10:17 +0100

    feat(refs DPLAN-12968, #AB21840): remove unnecessary line break

--- src/components/DpIcon/util/iconVariables.js
+++ src/components/DpIcon/util/iconVariables.js
@@ -166,7 +166,6 @@ const baseIcons = {
   square: {
     phosphorName: 'square',
     path: 'M208,32H48A16,16,0,0,0,32,48V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V48A16,16,0,0,0,208,32Zm0,176H48V48H208V208Z'
-
   },
   success: {
     phosphorName: 'check-circle',

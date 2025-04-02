d7e80b03fb075b274dd388f563285a6523e8c076
Natalija Sakuta <sakuta@demos-deutschland.de>
2025-04-02 11:36:27 +0200

    feat(DPLAN-15263): fix import

--- src/components/DpConfirmDialog/DpConfirmDialog.stories.tsx
+++ src/components/DpConfirmDialog/DpConfirmDialog.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/vue3'
+import type { Meta, StoryObj } from '@storybook/vue'
 import DpConfirmDialog from './'
 
 const meta: Meta<typeof DpConfirmDialog> = {

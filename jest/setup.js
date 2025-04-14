7d4aa5463727b871f560351ce1b5dbc25d5946dd
Florian Salis <salis@demos-deutschland.de>
2025-04-14 12:50:06 +0200

    fix: Mock Stickier propperly as Class

--- jest/setup.js
+++ jest/setup.js
@@ -14,6 +14,11 @@ jest.mock('@uppy/progress-bar', () => () => 'mock result')
 jest.mock('@uppy/tus', () => () => 'mock result')
 jest.mock('vue-omnibox', () => () => 'mock result')
 jest.mock('a11y-datepicker', () => () => 'mock result')
-jest.mock('~/lib/Stickier', () => () => 'mock result')
+jest.mock('~/lib/Stickier', () => jest.fn().mockImplementation(function() {
+  return {
+    update: jest.fn(),
+    destroy: jest.fn()
+  }
+}))
 
 

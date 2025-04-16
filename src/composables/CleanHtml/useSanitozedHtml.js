8ecf5bc32679c0a4e880f5a355732cb608f04fa3
riechedemos <carolin@demos-international.com>
2025-04-16 09:17:00 +0200

    feat(refs: DPLAN-15497 create composable for sanitizing html (refactor cleanhtml directive)

---
+++ src/composables/CleanHtml/useSanitozedHtml.js
@@ -0,0 +1,16 @@
+import { computed, isRef } from 'vue'
+import DOMPurify from 'dompurify'
+
+/**
+ * Composable to sanitize HTML content.
+ * @param {String|Ref<String>} rawHtml - The raw HTML content to sanitize.
+ * @param {Object} options - Optional DOMPurify configuration.
+ * @returns {ComputedRef<String>} - Sanitized HTML content.
+ */
+export function useSanitizedHtml(rawHtml, options = {}) {
+  return computed(() => {
+    const content = isRef(rawHtml) ? rawHtml.value : rawHtml
+    const _options = { ...options, ADD_ATTR: ['target'] }
+    return DOMPurify.sanitize(content, _options)
+  })
+}

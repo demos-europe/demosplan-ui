a94b6819abd80b9f54f995da92dd86799fe50173
Ania Rafeld <rafeld@demos-deutschland.de>
2025-06-10 15:33:41 +0200

    fix(refs DPLAN-15771): Adjust custom event name

--- src/components/DpDataTable/DpDataTable.vue
+++ src/components/DpDataTable/DpDataTable.vue
@@ -640,7 +640,7 @@ export default {
       }), status)
 
       // Used by multi-page selection in SegmentsList to determine whether to track selected or deselected items.
-      this.$emit('select-all', status)
+      this.$emit('selectAll', status)
     },
 
     toggleWrap (id) {

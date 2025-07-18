17d061fec0dd4d292f67a68f888be84f66de558c
Florian Salis <salis@demos-deutschland.de>
2025-07-18 13:58:11 +0200

    fix: add vite config

---
+++ vite.config.mjs
@@ -0,0 +1,44 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src')
+    },
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+  },
+  css: {
+    postcss: './postcss.config.mjs'
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.js'),
+      name: '__demos_europe_demosplan_ui',
+      fileName: 'demosplan-ui'
+    },
+    rollupOptions: {
+      external: [
+        'vue',
+        '@braintree/sanitize-url',
+        /^@uppy\/.+$/,
+        'dayjs',
+        'dompurify',
+        'lscache',
+        'plyr',
+        'tippy.js',
+        'uuid',
+        'v-tooltip',
+        'vue-multiselect',
+        'vuedraggable'
+      ],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+})

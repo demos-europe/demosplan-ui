bb8ab16a0b504c8d19b45fd50560ac17a3a7a10e
riechedemos <carolin@demos-international.com>
2025-12-09 12:55:59 +0100

    fix(refs: DPLAN-15175)  add blank line

--- src/components/DpDatepicker/DpDatepicker.vue
+++ src/components/DpDatepicker/DpDatepicker.vue
@@ -208,6 +208,7 @@ export default {
     this.addErrorFieldnameAttribute()
     this.setupLabelClickHandler()
   },
+
   beforeUnmount () {
     if (this.labelClickHandler) {
       const label = document.querySelector(`label[for="${this.id}"]`)

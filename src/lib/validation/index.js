df2cdc75541df9b36a5e614065269fceb923b5a1
Heidrun Wiemold <wiemold@demos-deutschland.de>
2026-02-04 09:25:03 +0100

    fix: use "export ... from"
    this fixes a sonarcloud issue:
    When re-exporting from a module, using separate import and export
    statements creates unnecessary verbosity and intermediate variables.
    The export…from syntax is more explicit about the intent to re-export.

--- src/lib/validation/index.js
+++ src/lib/validation/index.js
@@ -3,7 +3,6 @@ import { assignHandlersForInputs } from './utils/assignHandlersForInputs'
 import assignObserver from './utils/assignObserver'
 import dpValidate from './dpValidate'
 import dpValidateMultiselectDirective from './dpValidateMultiselectDirective'
-import { errorClass, scrollToVisibleElement } from './utils/helpers'
 import validateDatepicker from './utils/validateDatepicker'
 import validateEmail from './utils/validateEmail'
 import validateFieldset from './utils/validateFieldset'
@@ -12,14 +11,14 @@ import validateInput from './utils/validateInputField'
 import validateMultiselect from './utils/validateMultiselect'
 import validateTiptap from './utils/validateTiptap'
 
+export { errorClass, scrollToVisibleElement } from './utils/helpers'
+
 export {
   assignHandlerForTrigger,
   assignHandlersForInputs,
   assignObserver,
   dpValidate,
   dpValidateMultiselectDirective,
-  errorClass,
-  scrollToVisibleElement,
   validateDatepicker,
   validateEmail,
   validateFieldset,

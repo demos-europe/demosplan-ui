1a722a57550115721ccb56b0e34e9e48181d7e05
Natalija Sakuta <sakuta@demos-deutschland.de>
2026-04-08 16:22:47 +0200

    feat(DPLAN-17512): adjust DpConfirmDialog: add header and additional set up possibilities (new props)

--- src/components/DpConfirmDialog/DpConfirmDialog.vue
+++ src/components/DpConfirmDialog/DpConfirmDialog.vue
@@ -1,37 +1,60 @@
 <template>
   <dp-modal
     ref="confirmDialog"
-    content-classes="w-2/3 xl:w-1/3"
-    :data-cy="`confirmDialog:${dataCy}`"
     aria-describedby="dialogDescription"
+    content-classes="w-2/3 xl:w-1/3"
+    :content-header-classes="contentHeaderClasses"
+    :data-cy="`confirmDialog:${dataCy}`"
     @modal:toggled="onModalToggled"
   >
+    <template
+      v-if="header"
+      v-slot:header
+    >
+      <span>{{ header }}</span>
+    </template>
     <p
       id="dialogDescription"
       class="font-normal break-words mb-3"
     >
       {{ message }}
     </p>
-    <dp-button-row
-      primary
-      :primary-text="confirmButtonText"
-      secondary
-      :secondary-text="declineButtonText"
-      @primary-action="handleConfirm(true)"
-      @secondary-action="handleConfirm(false)"
-    />
+    <div class="flex flex-col items-end gap-3">
+      <dp-button-row
+        primary
+        :primary-text="confirmButtonText"
+        :primary-btn-variant="primaryBtnVariant"
+        secondary
+        :secondary-text="declineButtonText"
+        :secondary-btn-variant="secondaryBtnVariant"
+        @primary-action="handleConfirm(true)"
+        @secondary-action="handleConfirm(false)"
+      />
+      <dp-button
+        v-if="tertiaryButtonText"
+        :text="tertiaryButtonText"
+        color="warning"
+        variant="subtle"
+        @click="handleTertiaryAction"
+      />
+    </div>
   </dp-modal>
 </template>
 
 <script lang="ts">
 import { de } from '~/components/shared/translations'
+import DpButton from '~/components/DpButton'
 import DpButtonRow from '~/components/DpButtonRow'
 import DpModal from '~/components/DpModal'
+import { PropType } from 'vue'
+
+type ButtonVariant = 'solid' | 'outline' | 'subtle'
 
 export default {
   name: 'DpConfirmDialog',
 
   components: {
+    DpButton,
     DpButtonRow,
     DpModal,
   },
@@ -41,6 +64,12 @@ export default {
       type: String,
       required: false,
       default: de.operations.confirm,
+    },
+
+    contentHeaderClasses: {
+      required: false,
+      type: String,
+      default: '',
     },
 
     dataCy: {
@@ -55,12 +84,43 @@ export default {
       default: de.operations.abort,
     },
 
+    header: {
+      type: String,
+      required: false,
+      default: '',
+    },
+
     message: {
       type: String,
       required: false,
       default: de.confirmDialog.confirm,
     },
+
+    primaryBtnVariant: {
+      required: false,
+      type: String as PropType<ButtonVariant>,
+      default: 'solid',
+      validator: (prop: ButtonVariant) => ['solid', 'outline', 'subtle'].includes(prop),
+    },
+
+    secondaryBtnVariant: {
+      required: false,
+      type: String as PropType<ButtonVariant>,
+      default: 'solid',
+      validator: (prop: ButtonVariant) => ['solid', 'outline', 'subtle'].includes(prop),
+    },
+
+    tertiaryButtonText: {
+      type: String,
+      required: false,
+      default: '',
+    },
   },
+
+  emits: [
+    'confirmed',
+    'tertiary-action',
+  ],
 
   data () {
     return {
@@ -74,7 +134,14 @@ export default {
         /* Clear the reference to the resolver to avoid reusing it later */
         this.resolvePromise = null
       }
-      this.$refs.confirmDialog.toggle()
+
+      this.$emit('confirmed', isConfirmed)
+      this.$refs.confirmDialog?.toggle()
+    },
+
+    handleTertiaryAction () {
+      this.$emit('tertiary-action')
+      this.$refs.confirmDialog?.toggle()
     },
 
     onModalToggled (isOpen: boolean): void {

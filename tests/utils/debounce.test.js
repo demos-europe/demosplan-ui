2da829e8f8dda16a8684316161f30e50ad1a5298
Heidrun Wiemold <wiemold@demos-deutschland.de>
2026-04-28 14:24:57 +0200

    refactor(refs DPLAN-16770): improve test readability

--- tests/utils/debounce.test.js
+++ tests/utils/debounce.test.js
@@ -9,41 +9,37 @@ describe('debounce', () => {
      */
     vi.useFakeTimers()
   })
-  it('doesnt call the function if it has a waiting time', () => {
+  it('does not call the function if the debounce is reset within the cooldown time', () => {
     const callback = vi.fn()
     const debounced = debounce(callback, 500)
-    debounced(callback)
+
+    debounced()
+    vi.advanceTimersByTime(400)
+
+    debounced() // reset
+
+    vi.advanceTimersByTime(499) // still in cooldown (1ms left)
     expect(callback).not.toBeCalled()
 
-    vi.advanceTimersByTime(100)
-    debounced();
-    expect(callback).not.toBeCalled()
-
-    vi.advanceTimersByTime(499)
-    expect(callback).not.toBeCalled()
-
-    vi.advanceTimersByTime(300)
+    vi.advanceTimersByTime(1) // cooldown finished
     expect(callback).toBeCalledTimes(1)
   })
 
-  it('fires the callback, when immediate is true, no matter of the waiting time', () => {
-    const callback = vi.fn();
+  it('fires immediately and ignores calls within the cooldown time when immediate is true', () => {
+    const callback = vi.fn()
     const debounced = debounce(callback, 500, true)
-    expect(callback).not.toBeCalled()
+
     debounced()
-    expect(callback).toBeCalledTimes(1)
+    expect(callback).toBeCalledTimes(1) // immediate execution
 
-    vi.advanceTimersByTime(100)
     debounced()
+    vi.advanceTimersByTime(100) // still in cooldown (400ms left)
     expect(callback).toBeCalledTimes(1)
 
-    vi.advanceTimersByTime(499)
+    vi.advanceTimersByTime(399) // just before cooldown ends (1ms left)
     expect(callback).toBeCalledTimes(1)
 
-    vi.advanceTimersByTime(1)
-    expect(callback).toBeCalledTimes(1)
-
-    vi.advanceTimersByTime(700)
+    vi.advanceTimersByTime(1) // cooldown finished
     debounced()
     expect(callback).toBeCalledTimes(2)
   })

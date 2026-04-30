106f850b919ed79a15ec4e075d86f596ad0571c9
riechedemos <carolin@demos-international.com>
2026-04-30 09:48:11 +0200

    add test for attributes

--- tests/DpIcon.spec.js
+++ tests/DpIcon.spec.js
@@ -2,30 +2,43 @@ import DpIcon from '~/components/DpIcon/DpIcon.vue'
 import { shallowMount } from '@vue/test-utils'
 
 describe('DpIcon', () => {
-  it.each(['check', 'close', 'edit'])(
-    'renders correct icon for icon "%s"',
-    (icon) => {
-      const wrapper = shallowMount(DpIcon, {
-        props: { icon },
-      })
-      expect(wrapper.props('icon')).toBe(icon)
+  it.each([
+    ['small', '16'],
+    ['medium', '20'],
+    ['large', '24'],
+    ['xlarge', '32'],
+  ])('maps size "%s" to %s pixels', (size, expected) => {
+    const wrapper = shallowMount(DpIcon, {
+      props: { icon: 'check', size },
     })
+    expect(wrapper.attributes('size')).toBe(expected)
+  })
 
   it.each(['light', 'regular', 'bold', 'fill'])(
-    'renders icon with weight "%s"',
+    'sets weight attribute to "%s"',
     (weight) => {
       const wrapper = shallowMount(DpIcon, {
         props: { icon: 'check', weight },
       })
-      expect(wrapper.props('weight')).toBe(weight)
+      expect(wrapper.attributes('weight')).toBe(weight)
     })
 
-  it.each(['small', 'medium', 'large', 'xlarge'])(
-    'renders icon with size "%s"',
-    (size) => {
-      const wrapper = shallowMount(DpIcon, {
-        props: { icon: 'check', size },
-      })
-      expect(wrapper.props('size')).toBe(size)
+  it.each([
+    ['arrow-up', 'portrait'],
+    ['check', 'landscape'],
+    ['plus', 'square'],
+  ])('applies proportion class "%s" for icon "%s"', (icon, proportionClass) => {
+    const wrapper = shallowMount(DpIcon, {
+      props: { icon },
     })
+    expect(wrapper.classes()).toContain(proportionClass)
+  })
+
+  it('uses default size "medium" (20px) and weight "regular" when no props are given', () => {
+    const wrapper = shallowMount(DpIcon, {
+      props: { icon: 'check' },
+    })
+    expect(wrapper.attributes('size')).toBe('20')
+    expect(wrapper.attributes('weight')).toBe('regular')
+  })
 })

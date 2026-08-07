dd42bd30d01f0bb0927664a831f6076054162788
Lars Hüllner <huellner@demos-deutschland.de>
2026-08-07 12:24:42 +0200

    feat(refs: DPLAN-18152): emit orderedHeaderFields when columns-reordered

--- src/components/DpDataTable/DpDataTable.vue
+++ src/components/DpDataTable/DpDataTable.vue
@@ -499,6 +499,7 @@ export default {
 
   emits: [
     'changed-order',
+    'columns-reordered',
     'items-selected',
     'items-toggled',
     'selectAll',
@@ -661,6 +662,8 @@ export default {
           JSON.stringify(nonFixedOrder),
         )
       }
+
+      this.$emit('columns-reordered', this.orderedHeaderFields.map(headerField => headerField.field))
     },
 
     /**

969397cf3e6453aaa0754065b51d4ede6069f34e
riechedemos <carolin@demos-international.com>
2026-08-19 09:50:17 +0200

    feat(refs: DPLAN-18271): add a render component

---
+++ src/components/DpEditor/DpLinkedBoilerplate.vue
@@ -0,0 +1,23 @@
+<template>
+  <node-view-wrapper
+    ref="linkedBoilerplateWrapper"
+    as="div"
+    class="border-l-2 border-interactive hover:bg-surface-light"
+    v-tooltip="de.linkedBoilerplate.hint"
+  >
+    <node-view-content
+      ref="linkedBoilerplateContent"
+      class="p-2"
+    >
+    </node-view-content>
+  </node-view-wrapper>
+</template>
+<script setup>
+
+import { nodeViewProps, NodeViewContent, NodeViewWrapper } from '@tiptap/vue-3'
+import prefixClass from '~/utils/prefixClass'
+import { Tooltip as vTooltip } from '~/directives'
+import { de } from '../shared/translations'
+
+defineProps(nodeViewProps)
+</script>

04957329273f923fa3a524f99c6ccbda4d284958
riechedemos <carolin@demos-international.com>
2026-08-19 14:17:22 +0200

    feat(refs: DPLAN-18271): display resolved boilerplate title in the node view

--- src/components/DpEditor/DpLinkedBoilerplate.vue
+++ src/components/DpEditor/DpLinkedBoilerplate.vue
@@ -1,23 +1,33 @@
 <template>
   <node-view-wrapper
     ref="linkedBoilerplateWrapper"
+    v-tooltip="de.editor.linkedBoilerplate.hint"
     as="div"
     class="border-l-2 border-interactive hover:bg-surface-light"
-    v-tooltip="de.linkedBoilerplate.hint"
   >
+    <div class="grid grid-cols-[auto_minmax(0,1fr)_auto] items-center gap-1 px-2 pt-2 text-interactive">
+      <dp-icon
+        icon="puzzle-piece"
+        weight="fill"
+      />
+      <span class="truncate min-w-0">{{ de.editor.linkedBoilerplate.label({ title: props.extension.options.getBoilerplateTitle(props.node.attrs.boilerplateId) }) }}</span>
+      <dp-icon
+        icon="pencil-simple"
+        weight="fill"
+      />
+    </div>
     <node-view-content
       ref="linkedBoilerplateContent"
       class="p-2"
-    >
-    </node-view-content>
+    />
   </node-view-wrapper>
 </template>
 <script setup>
 
 import { nodeViewProps, NodeViewContent, NodeViewWrapper } from '@tiptap/vue-3'
-import prefixClass from '~/utils/prefixClass'
 import { Tooltip as vTooltip } from '~/directives'
 import { de } from '../shared/translations'
+import { DpIcon } from '~/components'
 
-defineProps(nodeViewProps)
+const props = defineProps(nodeViewProps)
 </script>
